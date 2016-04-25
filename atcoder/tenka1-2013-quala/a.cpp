73c81ee7e9556b9281d81eda9faa19b2d5db4ea2
imulan <k0223.teru@gmail.com>
2016-04-25 18:45:57 +0900

    0425

---
+++ atcoder/tenka1-2013-quala/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    long now=84;
+    while(now<130000000) now*=2;
+    cout << now <<endl;
+	return 0;
+}
