bfbbd7580c0467ff0f69e25ce96228d84be6196c
imulan <k0223.teru@gmail.com>
2016-05-29 22:40:22 +0900

    Solve

---
+++ yukicoder/369.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    int n;
+    cin >>n;
+
+    int ans=0;
+    rep(i,n)
+    {
+        int a;
+        scanf(" %d", &a);
+        ans+=a;
+    }
+
+    int v;
+    cin >>v;
+
+    cout << ans-v << endl;
+    return 0;
+}
