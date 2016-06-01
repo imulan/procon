6442f7534ee7e0a7b86444f7aeddfdf96a349d2e
imulan <k0223.teru@gmail.com>
2016-06-01 23:08:55 +0900

    Solve A

--- atcoder/birthday0410/a.py
+++ atcoder/birthday0410/a.py
@@ -1,11 +1,11 @@
 t=int(input())
 
 if t==1:
-	print(1,-1)
-	print(0,0)
+    print(1,-1)
+    print(0,0)
 elif t==2:
-
-	print(0,0)
-
+    print(-2147483648,0)
+    print(0,0)
 else:
-	print(0,0)
+    print(-1,1)
+    print(0,0)
