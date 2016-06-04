7a2b2f4e149a6cd765fd6e8e73f6da627c9d437d
imulan <k0223.teru@gmail.com>
2016-06-05 00:41:05 +0900

    modified

--- lib/FordFulkerson.cpp
+++ lib/FordFulkerson.cpp
@@ -26,39 +26,39 @@ bool used[MAX_V];
 
 //fromからtoへ向かう容量capの辺をグラフに追加する
 void add_edge(ff_t from, ff_t to, ff_t cap){
-  G[from].push_back((edge){to, cap, G[to].size()});
-  G[to].push_back((edge){from, 0, G[from].size()-1});
+    G[from].push_back((edge){to, cap, G[to].size()});
+    G[to].push_back((edge){from, 0, G[from].size()-1});
 }
 
 //増加パスを探す
 ff_t ff_dfs(ff_t v, ff_t t, ff_t f){
-  if(v==t) return f;
-  used[v]=true;
-  
-  for(ff_t i=0; i<G[v].size(); ++i){
-    edge &e=G[v][i];
-    if(!used[e.to] && e.cap>0){
-      ff_t d = ff_dfs(e.to, t, min(f,e.cap));
-      if(d>0){
-        e.cap -= d;
-        G[e.to][e.rev].cap += d;
-        return d;
-      }
+    if(v==t) return f;
+    used[v]=true;
+
+    for(ff_t i=0; i<G[v].size(); ++i){
+        edge &e=G[v][i];
+        if(!used[e.to] && e.cap>0){
+            ff_t d = ff_dfs(e.to, t, min(f,e.cap));
+            if(d>0){
+                e.cap -= d;
+                G[e.to][e.rev].cap += d;
+                return d;
+            }
+        }
     }
-  }
 
   return 0;
 }
 
 //sからtへの最大流を求める
 ff_t max_flow(ff_t s, ff_t t){
-  ff_t flow=0;
-  for(;;){
-    memset(used,0,sizeof(used));
-    ff_t f = ff_dfs(s,t,FF_INF);
-    if(f==0) return flow;
-    flow+=f;
-  }
+    ff_t flow=0;
+    for(;;){
+        memset(used,0,sizeof(used));
+        ff_t f = ff_dfs(s,t,FF_INF);
+        if(f==0) return flow;
+        flow+=f;
+    }
 }
 
 /******************************
--- lib/MaxSegTree.cpp
+++ lib/MaxSegTree.cpp
@@ -7,38 +7,38 @@
 ******************************/
 
 struct MaxSegTree{
-	long n; vector<ll> dat;
-	//初期化
-	MaxSegTree(long _n){
-		n=1;
-		while(n<_n) n*=2;
-		dat=vector<ll>(2*n-1,LLONG_MIN);
-	}
-	//k番目(0-indexed)の値をaに変更
-	void update(long k, ll a){
-		k+=n-1;
-		dat[k]=a;
-		//更新
-		while(k>0){
-		k=(k-1)/2;
-		dat[k]=max(dat[2*k+1],dat[2*k+2]);
-		}
-	}
-	//内部的に投げられるクエリ
-	ll _query(long a, long b, long k, long l, long r){
-		if(r<=a || b<=l) return LLONG_MIN;
+    long n; vector<ll> dat;
+    //初期化
+    MaxSegTree(long _n){
+        n=1;
+        while(n<_n) n*=2;
+        dat=vector<ll>(2*n-1,LLONG_MIN);
+    }
+    //k番目(0-indexed)の値をaに変更
+    void update(long k, ll a){
+        k+=n-1;
+        dat[k]=a;
+        //更新
+        while(k>0){
+            k=(k-1)/2;
+            dat[k]=max(dat[2*k+1],dat[2*k+2]);
+        }
+    }
+    //内部的に投げられるクエリ
+    ll _query(long a, long b, long k, long l, long r){
+        if(r<=a || b<=l) return LLONG_MIN;
 
-		if(a<=l && r<=b) return dat[k];
-		else{
-			ll vl=_query(a,b,2*k+1,l,(l+r)/2);
-			ll vr=_query(a,b,2*k+2,(l+r)/2,r);
-			return max(vl,vr);
-		}
-	}
-	//[a,b)の最大値を求める
-	ll query(long a, long b){
-		return _query(a,b,0,0,n);
-	}
+        if(a<=l && r<=b) return dat[k];
+        else{
+            ll vl=_query(a,b,2*k+1,l,(l+r)/2);
+            ll vr=_query(a,b,2*k+2,(l+r)/2,r);
+            return max(vl,vr);
+        }
+    }
+    //[a,b)の最大値を求める
+    ll query(long a, long b){
+        return _query(a,b,0,0,n);
+    }
 };
 
 /******************************
