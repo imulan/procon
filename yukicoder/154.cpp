1e459f905bacb110121206968100e99951edb40d
imulan <k0223.teru@gmail.com>
2016-06-20 15:08:22 +0900

    Solve 154

--- yukicoder/154.cpp
+++ yukicoder/154.cpp
@@ -1,56 +1,93 @@
-//BRの後にWが来てはいけないとか、もっと考えないとダメ
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <map>
-#include <algorithm>
-#include <set>
-#include <sstream>
-#include <utility>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <cctype>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n) for(int i=0;i<(n);++i)
-#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
 
-int main(int argc, char const *argv[]) {
-  int t;
-  cin >>t;
-  REP(T,t){
-    string s;
-    cin >>s;
+int main()
+{
+    int T;
+    cin >>T;
+    rep(times,T)
+    {
+        string s;
+        cin >>s;
 
-    int g=0,r=0,w=0;
-    bool v=true;
-    REP(i,s.size()){
-      if(s[i]=='G') ++g;
-      else if(s[i]=='R') ++r;
-      else ++w;
+        int n=s.size();
 
-      if(r>g){
-        v=false;
-        break;
-      }
+        //明らかに不可能な例
+        if(n<3)
+        {
+            printf("impossible\n");
+            continue;
+        }
+        else
+        {
+            if(s[0]!='W' || s[n-1]!='R')
+            {
+                printf("impossible\n");
+                continue;
+            }
+        }
+
+        int w=0,g=0,r=0;
+        rep(i,n)
+        {
+            if(s[i]=='W') ++w;
+            else if(s[i]=='G') ++g;
+            else ++r;
+        }
+
+        //この時だけ条件を満たす可能性がある
+        if(g==r && w>=g)
+        {
+            bool valid=true;
+
+            //cg,crは常にcw以下
+            //cgがgになったらそれ以降wが来てはいけない
+            //crがcgより多くなることはない
+            int cw=0,cg=0,cr=0;
+            rep(i,n)
+            {
+                if(s[i]=='W')
+                {
+                    ++cw;
+                    if(cg==g)
+                    {
+                        valid=false;
+                        break;
+                    }
+                }
+                else if(s[i]=='G')
+                {
+                    ++cg;
+                    if(cg>cw)
+                    {
+                        valid=false;
+                        break;
+                    }
+                }
+                else
+                {
+                    ++cr;
+                    if(cr>cg || cr>cw)
+                    {
+                        valid=false;
+                        break;
+                    }
+                }
+            }
+
+            if(valid) printf("possible\n");
+            else printf("impossible\n");
+        }
+        else printf("impossible\n");
     }
-
-    if(g!=r) v=false;
-    else{
-      if(g==0) v=false;
-      if(g>w) v=false;
-    }
-    string ans="possible";
-    if(!v) ans="im"+ans;
-    std::cout << ans << std::endl;
-  }
-  return 0;
+    return 0;
 }
