349335104f874d37af94a7cb3cd7371e33ecc0c5
imulan <k0223.teru@gmail.com>
2016-06-21 13:10:29 +0900

    Solve 379

---
+++ yukicoder/379.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    int n;
+    double g,v;
+    cin >>n >>g >>v;
+    n/=5;
+
+    printf("%.20lf\n",g*n/v);
+    return 0;
+}
