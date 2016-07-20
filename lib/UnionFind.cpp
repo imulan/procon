beef18e04a2032d6cbba19ae0017b4801d0aaeff
imulan <k0223.teru@gmail.com>
2016-07-21 01:24:25 +0900

    Modify Unionfind

--- lib/UnionFind.cpp
+++ lib/UnionFind.cpp
@@ -3,18 +3,18 @@ struct UF{
     //正だったらその頂点の親,負だったら根で連結成分の個数
     vector<int> d;
     UF() {}
-    UF(int n):n(n), d(n,-1){}
+    UF(int N):n(N), d(N,-1){}
     int root(int v){
         if(d[v]<0) return v;
         return d[v]=root(d[v]);
     }
-    bool unite(int x,int y){
-        x=root(x); y=root(y);
-        if(x==y) return false;
-        if(size(x) < size(y)) swap(x,y);
-        d[x]+=d[y];
-        d[y]=x;
+    bool unite(int X,int Y){
+        X=root(X); Y=root(Y);
+        if(X==Y) return false;
+        if(size(X) < size(Y)) swap(X,Y);
+        d[X]+=d[Y];
+        d[Y]=X;
         return true;
     }
     int size(int v){ return -d[root(v)]; }
-}
+};
