721973849f41b3ced66eaf76203e8a5785a1f99c
imulan <k0223.teru@gmail.com>
2016-07-26 17:05:55 +0900

    Solve 400

---
+++ yukicoder/400.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    string s;
+    cin >>s;
+
+    reverse(all(s));
+    rep(i,s.size())
+    {
+        if(s[i]=='<') s[i]='>';
+        else s[i]='<';
+    }
+
+    cout << s << endl;
+    return 0;
+}
