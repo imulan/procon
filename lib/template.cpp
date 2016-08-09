8f8cec39cc73bdd6a5962b632de1d47749b20c9f
imulan <k0223.teru@gmail.com>
2016-08-10 02:09:47 +0900

    Remove make_pair macro

--- lib/template.cpp
+++ lib/template.cpp
@@ -5,7 +5,6 @@ typedef long long ll;
 #define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
 #define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
 #define all(x) (x).begin(),(x).end()
-#define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
