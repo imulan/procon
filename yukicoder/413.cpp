002a756042f4c41750513cfeba401cfb2f30a23a
imulan <k0223.teru@gmail.com>
2016-08-13 04:45:30 +0900

    Add 413

---
+++ yukicoder/413.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    rep(i,100000)
+    {
+        ll k=100000000+i;
+        ll d=k*k+k-1;
+        cout << d << endl;
+    }
+    return 0;
+}
