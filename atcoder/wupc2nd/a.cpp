1069975b175bd9ea2c3436b7877d59170bb64a19
imulan <k0223.teru@gmail.com>
2016-08-19 00:26:56 +0900

    Solve A

---
+++ atcoder/wupc2nd/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    int n,m;
+    cin >>n >>m;
+
+    int a=n*(n+1)*(2*n+1)/6;
+    cout << a%m << endl;
+    return 0;
+}
