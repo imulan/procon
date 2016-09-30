20f1a8643002405df1a9c6e7006f6fea9a9c902d
imulan <k0223.teru@gmail.com>
2016-10-01 04:54:32 +0900

    Solve 721C

--- codeforces/721C.cpp
+++ codeforces/721C.cpp
@@ -9,18 +9,51 @@ typedef long long ll;
 #define fi first
 #define se second
 
-const int INF=123456789;
+const int INF=1000000002;
 
 typedef pair<int,int> pi;
 
 struct edge{int to,cost;};
-vector<edge> G[5000],invG[5000];
+vector<edge> G[5000];
 
-map<pi,int> dp;
+int n,m,T;
+
+int dp[5005][5005];
+int nx[5005][5005];
+
+int dfs(int now, int num)
+{
+    if(dp[now][num]>=0) return dp[now][num];
+
+    int ret=INF;
+    if(num==1)
+    {
+        if(now==n-1) return 0;
+        return INF;
+    }
+    else
+    {
+        rep(i,G[now].size())
+        {
+            edge e=G[now][i];
+            // printf(" call %d, %d\n", e.to, num);
+            int v=dfs(e.to,num-1);
+
+            if(ret>v+e.cost && v+e.cost<=T)
+            {
+                ret=v+e.cost;
+                nx[now][num]=e.to;
+                // printf("nx[%d][%d]=%d\n", now,num,e.to);
+            }
+        }
+
+    }
+
+    return dp[now][num]=min(INF,ret);
+}
 
 int main()
 {
-    int n,m,T;
     scanf(" %d %d %d", &n, &m, &T);
     rep(i,m)
     {
@@ -29,69 +62,38 @@ int main()
         --u;
         --v;
         G[u].pb(edge{v,t});
-        invG[v].pb(edge{u,t});
     }
 
-    dp[pi(0,1)]=0;
-    queue<pi> que;
-    que.push(pi(0,1));
-    while(!que.empty())
-    {
-        pi now=que.front();
-        que.pop();
-        int v=now.fi;
-        int num=now.se;
-        // printf(" now %d\n", v);
+    memset(dp,-1,sizeof(dp));
+    memset(nx,-1,sizeof(nx));
 
-        rep(i,G[v].size())
-        {
-            edge e=G[v][i];
-            if(dp.find(pi(e.to,num+1))==dp.end() || dp[pi(e.to,num+1)]>dp[now]+e.cost)
-            {
-                dp[pi(e.to,num+1)]=dp[now]+e.cost;
-                que.push(pi(e.to,num+1));
-                // printf("update %d\n", dp[pi(e.to,num+1)]);
-
-            }
-        }
-    }
-
-    int ans=0;
+    int ans=n-1;
     for(int i=n; i>=2; --i)
     {
-        if(dp.find(pi(n-1,i))!=dp.end() && dp[pi(n-1,i)]<=T)
+        int val=dfs(0,i);
+        if(val>=0 && val<=T)
         {
             ans=i;
-            // printf("dp %d = %d\n", i,dp[pi(n-1,i)]);
             break;
         }
     }
 
-    int cur=n-1;
+    int cur=0;
     int ct=ans;
     vector<int> a;
     while(1)
     {
-        // printf("cur =%d\n", cur);
+        // printf("cur,ct = %d , %d\n", cur,ct);
         a.pb(cur);
-        if(cur==0) break;
+        if(cur==n-1) break;
 
-        rep(i,invG[cur].size())
-        {
-            edge e=invG[cur][i];
-            // printf(" ? %d, %d -> %d\n", e.to, dp[pi(cur,ct)], dp[pi(e.to,ct-1)]);
-            if(dp[pi(cur,ct)]-e.cost == dp[pi(e.to,ct-1)])
-            {
-                cur=e.to;
-                --ct;
-                break;
-            }
-        }
+        cur=nx[cur][ct];
+        --ct;
     }
-    reverse(all(a));
+    // reverse(all(a));
 
     printf("%d\n", ans);
-    rep(i,ans)
+    rep(i,a.size())
     {
         if(i) printf(" ");
         printf("%d", a[i]+1);
