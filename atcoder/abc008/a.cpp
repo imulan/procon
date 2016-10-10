a918f59c43742058900c774fb25347e9e3a90207
imulan <k0223.teru@gmail.com>
2016-10-11 03:04:03 +0900

    Convert hard tab into soft tab

--- atcoder/abc008/a.cpp
+++ atcoder/abc008/a.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-	int s, t;
-	scanf(" %d %d", &s, &t);
-	printf("%d\n", t-s+1);
-	return 0;
+    int s, t;
+    scanf(" %d %d", &s, &t);
+    printf("%d\n", t-s+1);
+    return 0;
 }
