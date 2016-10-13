a5af28119642924abb1fe6bb934af77861c747e8
imulan <k0223.teru@gmail.com>
2016-10-13 16:46:59 +0900

    Solve 2395

---
+++ poj/2395.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+typedef pair<int,int> pi;
+typedef pair<int,pi> edge;
+
+struct UF{
+    int n;
+    //正だったらその頂点の親,負だったら根で連結成分の個数
+    vector<int> d;
+    UF() {}
+    UF(int N):n(N), d(N,-1){}
+    int root(int v){
+        if(d[v]<0) return v;
+        return d[v]=root(d[v]);
+    }
+    bool unite(int X,int Y){
+        X=root(X); Y=root(Y);
+        if(X==Y) return false;
+        if(size(X) < size(Y)) swap(X,Y);
+        d[X]+=d[Y];
+        d[Y]=X;
+        return true;
+    }
+    int size(int v){ return -d[root(v)]; }
+    bool same(int X,int Y){ return root(X)==root(Y); }
+};
+
+int main()
+{
+    int n,m;
+    scanf(" %d %d", &n, &m);
+
+    vector<edge> e(m);
+    rep(i,m)
+    {
+        int a,b,c;
+        scanf(" %d %d %d", &a, &b, &c);
+        --a;
+        --b;
+        e[i]=edge(c,pi(a,b));
+    }
+
+    sort(all(e));
+
+    UF uf(n);
+    int ans=0;
+    rep(i,m)
+    {
+        int a=e[i].se.fi, b=e[i].se.se;
+        if(!uf.same(a,b))
+        {
+            uf.unite(a,b);
+            ans=e[i].fi;
+        }
+
+        if(uf.size(0)==n) break;
+    }
+
+    printf("%d\n", ans);
+    return 0;
+}
