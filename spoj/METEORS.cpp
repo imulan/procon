cfd75fef84f9919e0a7bd537a84a7c4ff8141f57
imulan <k0223.teru@gmail.com>
2016-10-14 06:18:57 +0900

    Remove debug prints

--- spoj/METEORS.cpp
+++ spoj/METEORS.cpp
@@ -23,7 +23,6 @@ struct SumSegTree{
         while(idx>0){
             idx=(idx-1)/2;
             ret+=dat[idx];
-            // if(idx==0) break;
         }
         return ret;
     }
@@ -31,11 +30,7 @@ struct SumSegTree{
     void _query(int a, int b, ll v, int k, int l, int r){
         if(r<=a || b<=l) return;
 
-        if(a<=l && r<=b)
-        {
-            // printf("[%d,%d) -> +%lld\n", l,r,v);
-            dat[k]+=v;
-        }
+        if(a<=l && r<=b) dat[k]+=v;
         else{
             _query(a,b,v,2*k+1,l,(l+r)/2);
             _query(a,b,v,2*k+2,(l+r)/2,r);
@@ -43,7 +38,6 @@ struct SumSegTree{
     }
     //[a,b)に+v
     void add(int a, int b, ll v){
-        // printf("add query(%d, %d)\n", a,b);
         _query(a,b,v,0,0,n);
     }
 };
@@ -89,7 +83,6 @@ int main()
 
     rep(T,20)
     {
-        // printf(" %d iteration\n", T);
         vector<int> q[N];
         rep(i,n) q[(L[i]+R[i])/2].pb(i);
 
@@ -103,9 +96,6 @@ int main()
                 st.add(0,r[i]+1,a[i]);
             }
 
-            // printf(" %d addend\n", i);
-            // rep(j,m) printf(" %d  get %lld\n", j,st.get(j));
-
             rep(j,q[i].size())
             {
                 int s=q[i][j];
@@ -113,19 +103,15 @@ int main()
                 ll tmp=0;
                 rep(x,state[s].size())
                 {
-                    // printf(" access %d\n", state[s][x]);
                     tmp+=st.get(state[s][x]);
                     if(target[s]<=tmp) break;
                 }
-                // printf("state :%d, tmp %lld\n", s,tmp);
 
                 if(target[s]<=tmp) R[s]=i;
                 else L[s]=i;
             }
         }
     }
-
-    // rep(i,n)printf("L,R %d, %d\n", L[i],R[i]);
 
     rep(i,n)
     {
