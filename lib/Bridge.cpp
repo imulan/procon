50fb25e5248c2cd645d8051c6dd968d84e7fdce7
imulan <k0223.teru@gmail.com>
2016-11-14 04:59:23 +0900

    Add Bridge.cpp

---
+++ lib/Bridge.cpp
@@ -0,0 +1,38 @@
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int,int> edge;
+
+vector<vi> G;
+
+void bridge_dfs(int now, int from, vector<edge> &bridge, vector<vi> &bicomp, stack<int> &roots, stack<int> &S, vb &inS, vi &preord, int &pre_ct){
+    preord[now] = pre_ct++;
+    S.push(now); inS[now]=true;
+    roots.push(now);
+    rep(i,G[now].size()){
+        int to = G[now][i];
+        if(preord[to] == -1) bridge_dfs(to,now,bridge,bicomp,roots,S,inS,preord,pre_ct);
+        else if(from != to && inS[to]) while(preord[roots.top()] > preord[to]) roots.pop();
+    }
+    if(now == roots.top()){
+        bridge.pb(edge(now,from));
+        bicomp.pb(vi());
+        while(1){
+            int v=S.top(); S.pop(); inS[v]=false;
+            bicomp.back().pb(v);
+            if(now == v) break;
+        }
+        roots.pop();
+    }
+}
+
+void bridge_detect(vector<edge> &bridge, vector<vi> &bicomp){
+    const int n=G.size();
+    vi preord(n,-1);
+    vb inS(n,false);
+    stack<int> roots,S;
+    int pre_ct=0;
+    rep(i,n)if(preord[i] == -1){
+        bridge_dfs(i,n,bridge,bicomp,roots,S,inS,preord,pre_ct);
+        bridge.pop_back();
+    }
+}
