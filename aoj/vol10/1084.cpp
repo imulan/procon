eb0977cfd8e5cebc16a8010544eb439d9048a4d8
imulan <k0223.teru@gmail.com>
2016-11-16 05:35:47 +0900

    Solve 1084

--- aoj/vol10/1084.cpp
+++ aoj/vol10/1084.cpp
@@ -1,20 +1,49 @@
-#include <cstdio>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int n, k, c[100];
-	long teacher;
-	
-	scanf(" %d %d", &n, &k);
-	for(int i=0; i<n; ++i) scanf(" %d", &c[i]);
-	
-	for(int i=0; i<n; ++i){
-			
-		
-	}
-	
-	
-	
-	return 0;
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    int n,k;
+    while(scanf(" %d %d", &n, &k),n)
+    {
+        vector<int> c(n);
+        rep(i,n) scanf(" %d", &c[i]);
+
+        int t=0;
+        for(int i=0; i+k-1<n; ++i)
+        {
+            int tmp=1;
+            rep(j,k) tmp*=c[i+j];
+            t=max(t,tmp);
+        }
+
+        int ans=-1;
+        rep(x,n)rep(y,x)
+        {
+            swap(c[x],c[y]);
+
+            int m=0;
+            for(int i=0; i+k-1<n; ++i)
+            {
+                int tmp=1;
+                rep(j,k) tmp*=c[i+j];
+                m=max(m,tmp);
+            }
+            ans=max(ans,m-t);
+
+            swap(c[x],c[y]);
+        }
+
+        if(ans<0) printf("NO GAME\n");
+        else printf("%d\n", ans);
+    }
+    return 0;
 }
