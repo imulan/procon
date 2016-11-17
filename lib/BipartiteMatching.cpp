6a5d4a07b3fe1f65650b86aa66a42af8ba122954
imulan <k0223.teru@gmail.com>
2016-11-17 17:16:28 +0900

    Add BipartiteMatching.cpp

---
+++ lib/BipartiteMatching.cpp
@@ -0,0 +1,34 @@
+const int MAX_V = ;
+int V; //頂点数
+vector<int> G[MAX_V]; //グラフ
+int match[MAX_V]; // マッチングのペア
+bool used[MAX_V]; // dfsで既に調べたか
+
+void add_edge(int u, int v){
+    G[u].push_back(v); G[v].push_back(u);
+}
+
+// 増加パスの探索
+bool bp_dfs(int v){
+    used[v]=true;
+    rep(i,G[v].size()){
+        int u=G[v][i], w=match[u];
+        if(w<0 || (!used[w]&&bp_dfs(w))){
+            match[v]=u; match[u]=v;
+            return true;
+        }
+    }
+    return false;
+}
+
+int bipartite_matching(){
+    int ret=0;
+    memset(match,-1,sizeof(match));
+    rep(i,V){
+        if(match[i]<0){
+            memset(used,0,sizeof(used));
+            if(bp_dfs(i)) ++ret;
+        }
+    }
+    return ret;
+}
