5234a23489a7c13cdab52082f89a3aeaeeab454e
imulan <k0223.teru@gmail.com>
2016-11-22 05:50:13 +0900

    Solve 0211

--- aoj/vol2/0211.cpp
+++ aoj/vol2/0211.cpp
@@ -1,46 +1,40 @@
-#include <cstdio>
-#include <iostream>
-#include <set>
-#include <utility>
+#include <bits/stdc++.h>
 using namespace std;
 
-long gcd(long x, long y){
-	if(x<y)	swap(x,y);
-	
-	if(y==0) return x;
-	else return gcd(y, x%y);
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+inline ll lcm(ll x, ll y)
+{
+    return x/__gcd(x,y)*y;
 }
 
-long lcm(long x, long y){
-	return x*y/gcd(x,y);	
-}
+int main()
+{
+    int n;
+    while(cin >>n,n)
+    {
+        vector<ll> d(n),v(n);
+        rep(i,n) cin >>d[i] >>v[i];
 
-int main(){
-	while(1){
-		int n;
-		scanf(" %d", &n);
-		if(n==0) break;
-		
-		pair<long,long> p[10];
-		set<pair<long,long> > S;		
+        ll T=1;
+        rep(i,n)
+        {
+            ll G=__gcd(v[i],d[i]);
+            T=lcm(T,d[i]/G);
+        }
 
-		for(int i=0; i<n; ++i){
-			scanf(" %ld %ld", &p[i].first, &p[i].second);
-			
-			int div=gcd(p[i].first, p[i].second);
-			p[i].first /= div;
-			p[i].second /= div;
-			S.insert(p[i]);
-		}
-		
-		long T=p[0].first;
-		for(int i=1; i<n; ++i){
-			T = lcm(T, p[i].first);	
-		}
-		//printf("T= %ld\n", T);
-		//printf("size= %ld\n", S.size());
-		
-		if(S.size()==1) for(int i=0; i<n; ++i) printf("1\n");
-		else for(int i=0; i<n; ++i) printf("%ld\n", T/p[i].first*p[i].second);
-	}
+        vector<ll> ans(n);
+        rep(i,n) ans[i]=v[i]*T/d[i];
+
+        ll div=ans[0];
+        for(int i=1; i<n; ++i) div=__gcd(div,ans[i]);
+        rep(i,n) cout << ans[i]/div << endl;
+    }
+    return 0;
 }
