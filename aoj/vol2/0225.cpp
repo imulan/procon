a4e1b27140994be4e1d69dcfe93044ee508c3675
imulan <k0223.teru@gmail.com>
2016-11-22 19:31:35 +0900

    Solve 0225

--- aoj/vol2/0225.cpp
+++ aoj/vol2/0225.cpp
@@ -1,65 +1,83 @@
-/*先頭と最後尾が同じ単語が入ってきた時にどう処理するかが問題*/
-
-#include <cstdio>
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	while(1){
-		int n;
-		int head[26], tail[26], same[26]; //頭，尻，両方が同じ
-		
-		for(int i=0; i<26; ++i){
-			head[i]=tail[i]=same[i]=0;	
-		}
-		
-		scanf(" %d", &n);
-		if(n==0) break;
-		for(int i=0; i<n; ++i){
-			string s;
-			cin >> s;
-		
-			if(s[0]==s[s.size()-1]) same[s[0]-'a']++;
-			else{
-				head[s[0]-'a']++;
-				tail[s[s.size()-1]-'a']++;
-			}
-		}
-		
-		bool ans=true;
-		for(int i=0; i<26; ++i){
-			if(head[i] != tail[i]) ans=false;	
-		}
-		
-		int ct=0;
-		for(int i=0; i<26; ++i){
-			if(same[i]>0) ct++;	
-		}
-		
-		if(ans && ct==1){
-			for(int i=0; i<26; ++i){
-				if(same[i]>0){
-					if(same[i]==n || head[i]>0 ){}
-					else{
-						ans=false;
-						break;
-					}
-				}	
-			}
-		}
-		else if(ans && ct>=2){
-			for(int i=0; i<=26; ++i){
-				if(same[i]>0){
-					if(head[i]==0){
-						ans=false;
-						break;
-					}	
-				}	
-			}
-		}
-		
-		if(ans) printf("OK\n");
-		else printf("NG\n");		
-	}
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    cin.tie(0);ios::sync_with_stdio(false);
+
+    int G[26][26];
+    int exist[26];
+    int vis[26];
+
+    int n;
+    while(cin >>n,n)
+    {
+        memset(G,0,sizeof(G));
+        memset(exist,0,sizeof(exist));
+        memset(vis,0,sizeof(vis));
+
+        int in[26]={0}, out[26]={0};
+        rep(i,n)
+        {
+            string s;
+            cin >>s;
+
+            int S=s.size();
+            int a=s[0]-'a', b=s[S-1]-'a';
+            ++out[a];
+            ++in[b];
+            G[a][b]=1;
+            exist[a]=exist[b]=1;
+        }
+
+        rep(i,26)
+        {
+            if(exist[i])
+            {
+                queue<int> que;
+                que.push(i);
+                vis[i]=1;
+                while(!que.empty())
+                {
+                    int now=que.front(); que.pop();
+                    rep(j,26)
+                    {
+                        if(G[now][j]==0) continue;
+                        if(!vis[j])
+                        {
+                            vis[j]=1;
+                            que.push(j);
+                        }
+                    }
+                }
+                break;
+            }
+        }
+
+        bool connected=true;
+        rep(i,26)
+        {
+            if(exist[i] && !vis[i]) connected=false;
+        }
+
+        bool valid=false;
+        if(connected)
+        {
+            valid=true;
+            rep(i,26) if(in[i]!=out[i]) valid=false;
+        }
+
+        string ans="NG";
+        if(valid) ans="OK";
+        cout << ans << endl;
+    }
+    return 0;
 }
