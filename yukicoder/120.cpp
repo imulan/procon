27289cec7f3dabe9b709962c855c6734e166fe60
imulan <k0223.teru@gmail.com>
2016-12-08 16:05:22 +0900

    Solve 120

--- yukicoder/120.cpp
+++ yukicoder/120.cpp
@@ -2,23 +2,53 @@
 using namespace std;
 
 typedef long long ll;
-#define rep(i,n) for(i=0;i<n;++i)
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
 #define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
-#define mp make_pair
+#define all(x) (x).begin(),(x).end()
 #define pb push_back
 #define fi first
-#define sc second
+#define se second
 
-int main(int argc, char const *argv[]) {
-  int t;
-  cin >>t;
-  for(int T=0; T<t; ++T){
-    int i,j;
+typedef pair<int,int> pi;
 
-    int n;
-    cin >>n;
-    rep(i,n){}
+int main()
+{
+    int T;
+    scanf(" %d", &T);
+    while(T--)
+    {
+        int n;
+        scanf(" %d", &n);
 
-  }
-  return 0;
+        map<int,int> ct;
+        rep(i,n)
+        {
+            int L;
+            scanf(" %d", &L);
+            ++ct[L];
+        }
+
+        priority_queue<int> que;
+        for(const auto &x:ct) que.push(x.se);
+
+        int ans=0;
+        while(que.size()>=3)
+        {
+            int p[3];
+            rep(i,3)
+            {
+                p[i]=que.top();
+                que.pop();
+            }
+            rep(i,3)
+            {
+                --p[i];
+                if(p[i]>0) que.push(p[i]);
+            }
+            ++ans;
+        }
+
+        printf("%d\n", ans);
+    }
+    return 0;
 }
