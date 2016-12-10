fb64801f017383528839797d0fd14ae2b9160956
imulan <k0223.teru@gmail.com>
2016-12-10 22:51:11 +0900

    Add SCC

---
+++ lib/SCC.cpp
@@ -0,0 +1,35 @@
+int V; // 頂点数 TODO:initialize
+const int MAX_V = ; // 最大長点数 TODO:insert
+vector<int> G[MAX_V]; // 隣接リスト表現
+vector<int> rG[MAX_V]; // 逆辺を張ったグラフ
+vector<int> vs; // 帰りがけ順の並び
+bool used[MAX_V]; // 既に調べたか
+int cmp[MAX_V]; //属する強連結成分トポロジカル順序
+
+void add_edge(int from, int to){
+    G[from].push_back(to);
+    rG[to].push_back(from);
+}
+
+void dfs(int v){
+    used[v] = true;
+    rep(i,G[v].size())if(!used[G[v][i]]) dfs(G[v][i]);
+    vs.push_back(v);
+}
+
+void rdfs(int v, int k){
+    used[v]=true;
+    cmp[v]=k;
+    rep(i,rG[v].size())if(!used[rG[v][i]]) rdfs(rG[v][i],k++);
+}
+
+int scc(){
+    memset(used,0,sizeof(used));
+    vs.clear();
+    rep(v,V)if(!used[v]) dfs(v);
+
+    memset(used,0,sizeof(used));
+    int k=0;
+    for(int i=vs.size()-1; i>=0; --i)if(!used[vs[i]]) rdfs(vs[i],k++);
+    return k;
+}
