db7ae4f7118c6c9270973f09d777e04fec1aaa81
imulan <k0223.teru@gmail.com>
2016-12-15 18:00:40 +0900

    Add Binary Indexed Tree

---
+++ lib/BIT.cpp
@@ -0,0 +1,25 @@
+struct BIT{
+    // [1,n]
+    int n; vector<ll> bit;
+    // 初期化
+    BIT(int _n){
+        n = _n+1;
+        bit = vector<ll>(n,0);
+    }
+    // sum of [1,i]
+    ll sum(int i){
+        ll s = 0;
+        while(i>0){
+            s += bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+    // add x in i-th element
+    void add(int i, ll x){
+        while(i<=n){
+            bit[i] += x;
+            i += i & -i;
+        }
+    }
+};
