a05a851ffe1fab5ee2e1a51d42e71cc61a230aa0
imulan <k0223.teru@gmail.com>
2016-12-28 19:30:14 +0900

    Solve Div1 Easy

---
+++ topcoder/SRM/SRM 644/OkonomiyakiParty.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+const int mod=1e9+7;
+
+class OkonomiyakiParty {
+    public:
+    int count(vector<int> osize, int M, int K) {
+        int C[51][51];
+
+        rep(i,51)
+        {
+            C[i][0]=1;
+            C[i][i]=1;
+        }
+        for(int i=2; i<=50; ++i)for(int j=1; j<i; ++j) C[i][j]=(C[i-1][j]+C[i-1][j-1])%mod;
+
+        int n = osize.size();
+        sort(all(osize));
+
+        int ret=0;
+
+        rep(i,n-1)for(int j=i+1; j<n; ++j)
+        {
+            if(osize[j]-osize[i]<=K && j-1-i>=M-2) (ret+=C[j-1-i][M-2])%=mod;
+        }
+
+        return ret;
+    }
+};
+
+// CUT begin
+ifstream data("OkonomiyakiParty.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> osize, int M, int K, int __expected) {
+    time_t startClock = clock();
+    OkonomiyakiParty *instance = new OkonomiyakiParty();
+    int __result = instance->count(osize, M, K);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> osize;
+        from_stream(osize);
+        int M;
+        from_stream(M);
+        int K;
+        from_stream(K);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(osize, M, K, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1482920086;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "OkonomiyakiParty (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
