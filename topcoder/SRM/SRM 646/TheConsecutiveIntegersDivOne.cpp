705c4091c796e077d446b7291dacdc26686156c4
imulan <k0223.teru@gmail.com>
2016-12-28 17:03:02 +0900

    Solve Div1 Easy

---
+++ topcoder/SRM/SRM 646/TheConsecutiveIntegersDivOne.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+class TheConsecutiveIntegersDivOne {
+    public:
+    int find(vector<int> numbers, int k) {
+        sort(all(numbers));
+
+        int n=numbers.size();
+        ll ans=1234567890LL;
+
+        rep(i,n)
+        {
+            rep(j,k)
+            {
+                int st_idx = i-j;
+                if(st_idx<0) continue;
+
+                int ed_idx = st_idx+k-1;
+                if(ed_idx>=n) continue;
+
+                int v=numbers[i]-j;
+                ll t=0;
+                for(int x=st_idx; x<=ed_idx; ++x)
+                {
+                    t += abs(numbers[x]-v);
+                    ++v;
+                }
+
+                ans = min(ans,t);
+            }
+        }
+
+        return (int)ans;
+    }
+};
+
+// CUT begin
+ifstream data("TheConsecutiveIntegersDivOne.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> numbers, int k, int __expected) {
+    time_t startClock = clock();
+    TheConsecutiveIntegersDivOne *instance = new TheConsecutiveIntegersDivOne();
+    int __result = instance->find(numbers, k);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> numbers;
+        from_stream(numbers);
+        int k;
+        from_stream(k);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(numbers, k, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1482910933;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheConsecutiveIntegersDivOne (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
