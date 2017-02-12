b41448dbab1769452324c9025fa6bfb063d1e497
imulan <k0223.teru@gmail.com>
2017-02-13 04:13:45 +0900

    Solve Div1 Easy

---
+++ topcoder/SRM/SRM 643/TheKingsFactorization.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll LIM = 1000000000LL;
+const int S = 1000000;
+
+class TheKingsFactorization {
+    public:
+    bool prime[S+1]={};
+
+    vector<long long> getVector(long long N, vector<long long> primes) {
+        fill(prime,prime+S+1,true);
+        prime[0]=prime[1]=false;
+        for(int i=2; i<=S; ++i)
+        {
+            if(prime[i]) for(int j=2*i; j<=S; j+=i) prime[j]=false;
+        }
+
+        vector<ll> ret(primes);
+        int P = primes.size();
+
+        rep(i,P) N/=primes[i];
+
+        if(P==1)
+        {
+            if(N!=1) ret.pb(N);
+            return ret;
+        }
+
+        ll t=N;
+        int i=2;
+        while(i*i<=N && i<=primes[P-1])
+        {
+            while(t%i==0)
+            {
+                t/=i;
+                ret.pb(i);
+            }
+
+            ++i;
+        }
+        if(t>1) ret.pb(t);
+
+        sort(all(ret));
+        return ret;
+    }
+};
+
+// CUT begin
+ifstream data("TheKingsFactorization.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(long long N, vector<long long> primes, vector<long long> __expected) {
+    time_t startClock = clock();
+    TheKingsFactorization *instance = new TheKingsFactorization();
+    vector<long long> __result = instance->getVector(N, primes);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        long long N;
+        from_stream(N);
+        vector<long long> primes;
+        from_stream(primes);
+        next_line();
+        vector<long long> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, primes, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1486923906;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheKingsFactorization (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
