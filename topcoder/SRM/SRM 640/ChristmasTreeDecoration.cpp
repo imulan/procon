2c408f834964fc094d0749cacf4ca0a55037384e
imulan <k0223.teru@gmail.com>
2017-02-19 03:51:02 +0900

    Solve Div1 Easy

---
+++ topcoder/SRM/SRM 640/ChristmasTreeDecoration.cpp
@@ -0,0 +1,185 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+typedef pair<int,int> pi;
+
+struct UF{
+    int n;
+    //正だったらその頂点の親,負だったら根で連結成分の個数
+    vector<int> d;
+    UF() {}
+    UF(int N):n(N), d(N,-1){}
+    int root(int v){
+        if(d[v]<0) return v;
+        return d[v]=root(d[v]);
+    }
+    bool unite(int X,int Y){
+        X=root(X); Y=root(Y);
+        if(X==Y) return false;
+        if(size(X) < size(Y)) swap(X,Y);
+        d[X]+=d[Y];
+        d[Y]=X;
+        return true;
+    }
+    int size(int v){ return -d[root(v)]; }
+    bool same(int X,int Y){ return root(X)==root(Y); }
+};
+
+class ChristmasTreeDecoration {
+    public:
+    int solve(vector<int> col, vector<int> x, vector<int> y) {
+        int n = col.size();
+        int m = x.size();
+
+        vector<pi> s,d;
+
+        rep(i,m)
+        {
+            --x[i];
+            --y[i];
+            if(col[x[i]] == col[y[i]]) s.pb(pi(x[i],y[i]));
+            else d.pb(pi(x[i],y[i]));
+        }
+
+        UF uf(n);
+        int ret=0;
+
+        rep(i,d.size())
+        {
+            int a=d[i].fi, b=d[i].se;
+            if(!uf.same(a,b)) uf.unite(a,b);
+        }
+
+        rep(i,s.size())
+        {
+            int a=s[i].fi, b=s[i].se;
+            if(!uf.same(a,b))
+            {
+                uf.unite(a,b);
+                ++ret;
+            }
+        }
+
+        return ret;
+    }
+};
+
+// CUT begin
+ifstream data("ChristmasTreeDecoration.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> col, vector<int> x, vector<int> y, int __expected) {
+    time_t startClock = clock();
+    ChristmasTreeDecoration *instance = new ChristmasTreeDecoration();
+    int __result = instance->solve(col, x, y);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> col;
+        from_stream(col);
+        vector<int> x;
+        from_stream(x);
+        vector<int> y;
+        from_stream(y);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(col, x, y, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1487443016;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ChristmasTreeDecoration (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
