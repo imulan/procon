20e585f6440f2c4f1666c88243a35128536cb20f
imulan <k0223.teru@gmail.com>
2017-02-26 02:35:32 +0900

    Solve 485

---
+++ yukicoder/485.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    int a,b;
+    cin >>a >>b;
+
+    if(abs(b)%abs(a)==0) printf("%d\n", b/a);
+    else printf("NO\n");
+    return 0;
+}
