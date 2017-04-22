7695cadc76148d5ee81754999fcbfee79c932ca7
imulan <k0223.teru@gmail.com>
2017-04-22 19:06:47 +0900

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
+using ll = long long;
 #define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
-#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
 #define all(x) (x).begin(),(x).end()
 #define pb push_back
 #define fi first
