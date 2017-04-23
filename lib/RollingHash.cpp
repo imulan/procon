ca7569ae0624cf18d4363a014f144590272adbd8
imulan <k0223.teru@gmail.com>
2017-04-24 03:26:20 +0900

    Add RollingHash

---
+++ lib/RollingHash.cpp
@@ -0,0 +1,34 @@
+struct RollingHash{
+    static const int MD = 3;
+    const vector<ll> hash_base{1009,1021,1013};
+    const vector<ll> hash_mod{1000000009,1000000007,1000000021};
+
+    int n;
+    vector<ll> hs[MD], pw[MD];
+
+    RollingHash(){}
+    RollingHash(const string &s){
+        n = s.size();
+        for(int i=0; i<MD; ++i){
+            hs[i].assign(n+1,0);
+            pw[i].assign(n+1,0);
+            hs[i][0] = 0;
+            pw[i][0] = 1;
+            for(int j=0; j<n; ++j){
+                pw[i][j+1] = pw[i][j]*hash_base[i] % hash_mod[i];
+                hs[i][j+1] = (hs[i][j]*hash_base[i]+s[j]) % hash_mod[i];
+            }
+        }
+    }
+
+    ll hash_value(int l, int r, int i){
+        return ((hs[i][r] - hs[i][l]*pw[i][r-l])%hash_mod[i]+hash_mod[i])%hash_mod[i];
+    }
+
+    // 1-index
+    bool match(int l1, int r1, int l2, int r2){
+        bool ret = true;
+        for(int i=0; i<MD; ++i) ret &= (hash_value(l1-1,r1,i) == hash_value(l2-1,r2,i));
+        return ret;
+    }
+};
