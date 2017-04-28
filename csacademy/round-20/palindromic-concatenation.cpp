11a9c0d5a916de98e0d216bfa7bfb77d4a2791cb
imulan <k0223.teru@gmail.com>
2017-04-28 18:42:17 +0900

    Update palindromic-concatenation

--- csacademy/round-20/palindromic-concatenation.cpp
+++ csacademy/round-20/palindromic-concatenation.cpp
@@ -7,12 +7,9 @@ using ll = long long;
 #define fi first
 #define se second
 
-using P = pair<int,string>;
-
 struct RollingHash{
     static const int MD = 3;
-    const vector<ll> hash_base{1009,1021,1013};
-    const vector<ll> hash_mod{1000000009,1000000007,1000000021};
+    static const vector<ll> hash_base, hash_mod;
 
     int n;
     vector<ll> hs[MD], pw[MD];
@@ -49,6 +46,8 @@ struct RollingHash{
         return ret;
     }
 };
+const vector<ll> RollingHash::hash_base{1009,1021,1013};
+const vector<ll> RollingHash::hash_mod{1000000009,1000000007,1000000021};
 
 const int N=100000;
 vector<string> s[N+1];
@@ -68,53 +67,39 @@ int main()
     }
 
     map<vector<ll>,ll> ct;
-
     ll ans = 0;
-    for(int L=N; L>0; --L)if(s[L].size())
+
+    for(int L=N; L>0; --L)
     {
+        int SZ=s[L].size();
+        if(!SZ) continue;
+
+        vector<string> t(SZ);
+        vector<RollingHash> hh(SZ),th(SZ);
+        rep(i,SZ)
+        {
+            t[i]=s[L][i];
+            reverse(all(t[i]));
+
+            hh[i] = RollingHash(s[L][i]);
+            th[i] = RollingHash(t[i]);
+        }
+
         // 長さが同じもの
         map<vector<ll>,int> same;
-        rep(i,s[L].size())
-        {
-            string t=s[L][i];
-            reverse(all(t));
-
-            RollingHash th(t);
-            ++same[th.calc(1,L)];
-        }
-        rep(i,s[L].size())
-        {
-            string t=s[L][i];
-            reverse(all(t));
-
-            RollingHash hh(s[L][i]);
-            ans += same[hh.calc(1,L)]-(s[L][i]==t);
-        }
+        rep(i,SZ) ++same[th[i].calc(1,L)];
+        rep(i,SZ) ans += same[hh[i].calc(1,L)]-(s[L][i]==t[i]);
 
         // 長さが違うもの
-        rep(i,s[L].size())
-        {
-            string t=s[L][i];
-            reverse(all(t));
-
-            RollingHash th(t);
-            ans += ct[th.calc(1,L)];
-        }
+        rep(i,SZ) ans += ct[th[i].calc(1,L)];
 
         // ハッシュのカウントを更新
-        rep(i,s[L].size())
+        rep(i,SZ)for(int pl=1; pl<L; ++pl)
         {
-            string t=s[L][i];
-            reverse(all(t));
-
-            RollingHash hh(s[L][i]),th(t);
-            for(int pl=1; pl<L; ++pl)
-            {
-                // s[L][i]の先頭からpl文字が回文になっているか？
-                if(hh.calc(1,pl) == th.calc(L-pl+1,L)) ++ct[hh.calc(pl+1,L)];
-                // s[L][i]の末尾からpl文字が回文になっているか？
-                if(hh.calc(L-pl+1,L) == th.calc(1,pl)) ++ct[hh.calc(1,L-pl)];
-            }
+            // s[L][i]の先頭からpl文字が回文になっているか？
+            if(hh[i].calc(1,pl) == th[i].calc(L-pl+1,L)) ++ct[hh[i].calc(pl+1,L)];
+            // s[L][i]の末尾からpl文字が回文になっているか？
+            if(hh[i].calc(L-pl+1,L) == th[i].calc(1,pl)) ++ct[hh[i].calc(1,L-pl)];
         }
     }
 
