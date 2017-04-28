20b4fe246151cfe5bb0925760994be7dd64de882
imulan <k0223.teru@gmail.com>
2017-04-28 18:54:37 +0900

    Update

--- csacademy/round-20/palindromic-concatenation.cpp
+++ csacademy/round-20/palindromic-concatenation.cpp
@@ -17,32 +17,32 @@ struct RollingHash{
     RollingHash(){}
     RollingHash(const string &s){
         n = s.size();
-        for(int i=0; i<MD; ++i){
+        rep(i,MD){
             hs[i].assign(n+1,0);
             pw[i].assign(n+1,0);
             hs[i][0] = 0;
             pw[i][0] = 1;
-            for(int j=0; j<n; ++j){
+            rep(j,n){
                 pw[i][j+1] = pw[i][j]*hash_base[i] % hash_mod[i];
                 hs[i][j+1] = (hs[i][j]*hash_base[i]+s[j]) % hash_mod[i];
             }
         }
     }
 
+    // 1-index
     ll hash_value(int l, int r, int i){
         return ((hs[i][r] - hs[i][l]*pw[i][r-l])%hash_mod[i]+hash_mod[i])%hash_mod[i];
     }
 
-    // 1-index
     bool match(int l1, int r1, int l2, int r2){
         bool ret = true;
-        for(int i=0; i<MD; ++i) ret &= (hash_value(l1-1,r1,i) == hash_value(l2-1,r2,i));
+        rep(i,MD) ret &= (hash_value(l1-1,r1,i) == hash_value(l2-1,r2,i));
         return ret;
     }
 
     vector<ll> calc(int l, int r){
-        vector<ll> ret(3);
-        rep(i,3) ret[i]=hash_value(l-1,r,i);
+        vector<ll> ret(MD);
+        rep(i,MD) ret[i]=hash_value(l-1,r,i);
         return ret;
     }
 };
@@ -55,7 +55,7 @@ vector<string> s[N+1];
 int main()
 {
     cin.tie(0);ios::sync_with_stdio(false);
-    
+
     int n;
     cin >>n;
 
