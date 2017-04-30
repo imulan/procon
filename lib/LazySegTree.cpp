e151ca289a94d94442c602dc389eeea9948fcf8d
imulan <k0223.teru@gmail.com>
2017-05-01 00:21:06 +0900

    Initialize LazySegTree

---
+++ lib/LazySegTree.cpp
@@ -0,0 +1,71 @@
+// 区間add, 区間min
+struct LazySegTree{
+    int n; vector<ll> dat,lazy;
+    //初期化
+    LazySegTree(int _n){
+        n=1;
+        while(n<_n) n*=2;
+        dat=vector<ll>(2*n-1,0);
+        lazy=vector<ll>(2*n-1,0);
+    }
+
+    void show()
+    {
+        rep(j,dat.size()) printf(" %d: dat= %lld, lazy= %lld\n", j,dat[j],lazy[j]);
+    }
+
+    void setLazy(int k, ll v){
+        lazy[k] += v;
+        dat[k] += v;
+    }
+
+    void push(int k, int l, int r){
+        if(lazy[k]!=0){
+            setLazy(2*k+1,lazy[k]);
+            setLazy(2*k+2,lazy[k]);
+        }
+        lazy[k]=0;
+    }
+
+    void fix(int k, int l, int r){
+        dat[k]=min(dat[2*k+1],dat[2*k+2]);
+    }
+
+    ll merge(ll x, ll y){
+        return min(x,y);
+    }
+
+    //内部的に投げられるクエリ
+    void _add(int a, int b, ll x, int k, int l, int r){
+        if(r<=a || b<=l) return;
+        if(a<=l && r<=b){
+            setLazy(k,x);
+            return;
+        }
+
+        push(k,l,r);
+        _add(a,b,x,2*k+1,l,(l+r)/2);
+        _add(a,b,x,2*k+2,(l+r)/2,r);
+
+        fix(k,l,r);
+    }
+    //[a,b)に+x
+    void add(int a, int b, ll x){
+        return _add(a,b,x,0,0,n);
+    }
+
+    //内部的に投げられるクエリ
+    ll _query(int a, int b, int k, int l, int r){
+        if(r<=a || b<=l) return LLONG_MAX/2;
+        if(a<=l && r<=b) return dat[k];
+
+        push(k,l,r);
+        ll vl=_query(a,b,2*k+1,l,(l+r)/2);
+        ll vr=_query(a,b,2*k+2,(l+r)/2,r);
+        return merge(vl,vr);
+    }
+    //[a,b)の最小値
+    ll query(int a, int b){
+        return _query(a,b,0,0,n);
+    }
+};
