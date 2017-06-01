8210c7f6cff9ad9522f41545b45bc05912c690f2
imulan <k0223.teru@gmail.com>
2017-06-01 18:37:54 +0900

    Initialize Dinic

---
+++ lib/Dinic.cpp
@@ -0,0 +1,60 @@
+// (行き先,容量,逆辺)
+struct edge{ int to,cmp,rev; };
+
+const int MAX_V = ; // TODO:initialize
+const int F_INF = ; // TODO:initialize
+vector<edge> G[MAX_V];
+int level[MAX_V]; // sからの距離
+int iter[MAX_V]; // どこまで調べ終わったか
+
+void add_edge(int from, int to, int cap){
+    G[from].pb({to,cap,G[to].size()});
+    G[to].pb({from,0,G[from].size()-1});
+}
+
+void dinic_bfs(int s){
+    memset(level,-1,sizeof(level));
+    queue<int> que;
+    level[s]=0;
+    que.push(s);
+    while(!que.empty()){
+        int v = que.front();
+        que.pop();
+        rep(i,G[v].size()){
+            edge &e = G[v][i];
+            if(e.cap>0 && level[v] < level[e.to]){
+                level[e.to] = level[v]+1;
+                que.push(e.to);
+            }
+        }
+    }
+}
+
+// 増加パスをdfsで探す
+int dinic_dfs(int v, int t, int f){
+    if(v==t) return f;
+    for(int &i=iter[v]; i<G[v].size(); ++i){
+        edge e=G[v][i];
+        if(e.cap>0 && level[v]<level[e.to]){
+            int d = dinic_dfs(e.to,t,min(f,e.cap));
+            if(d>0){
+                e.cap -= d;
+                G[e.to][e.rev].cap += d;
+                return d;
+            }
+        }
+    }
+    return 0;
+}
+
+// sからtへの最大流
+int max_flow(int s, int t){
+    int flow = 0;
+    while(1){
+        dinic_bfs(s);
+        if(level[t]<0) return flow;
+        memset(iter,0,sizeof(iter));
+        int f;
+        while((f=dinic_dfs(s,t,F_INF))>0) flow+=f;
+    }
+}
