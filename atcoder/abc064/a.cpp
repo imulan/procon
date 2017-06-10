655627a8222d2ca8e2b95be82cd24c016cb30d7c
imulan <k0223.teru@gmail.com>
2017-06-11 00:05:45 +0900

    Solve a

---
+++ atcoder/abc064/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    int r,g,b;
+    cin >>r >>g >>b;
+    cout << (((100*r+10*g+b)%4==0)?"YES":"NO") << endl;
+    return 0;
+}
