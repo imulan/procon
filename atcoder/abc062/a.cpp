c4b261d1cc1f52fefa037ad96a7c09631c176af8
imulan <k0223.teru@gmail.com>
2017-06-13 04:18:26 +0900

    Solve a

---
+++ atcoder/abc062/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    int g[13]={};
+    g[4]=g[6]=g[9]=g[11]=1;
+    g[2]=2;
+
+    int x,y;
+    cin >>x >>y;
+    cout << (g[x]==g[y]?"Yes":"No") << endl;
+    return 0;
+}
