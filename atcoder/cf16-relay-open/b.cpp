e0bf54ca9ae4c83058e133147cbc91e3c4f72c33
imulan <k0223.teru@gmail.com>
2017-06-15 18:05:05 +0900

    Solve b

---
+++ atcoder/cf16-relay-open/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+string solve(string s)
+{
+    int S = s.size();
+    string NG = "No", OK = "Yes";
+    rep(i,S)
+    {
+        if(s[i]=='b' && s[S-1-i]!='d') return NG;
+        if(s[i]=='d' && s[S-1-i]!='b') return NG;
+        if(s[i]=='p' && s[S-1-i]!='q') return NG;
+        if(s[i]=='q' && s[S-1-i]!='p') return NG;
+    }
+    return OK;
+}
+
+int main()
+{
+    string s;
+    cin >>s;
+    cout << solve(s) << endl;
+    return 0;
+}
