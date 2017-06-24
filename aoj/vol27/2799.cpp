9084bf7197305d694547ae552296927c8ed1a49c
imulan <k0223.teru@gmail.com>
2017-06-24 19:19:48 +0900

    Solve 2799

---
+++ aoj/vol27/2799.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    int n,d;
+    cin >>n >>d;
+    int a = 0;
+    rep(i,n)
+    {
+        int p;
+        cin >>p;
+        if(p-d>0) a+=p-d;
+    }
+
+    if(!a) cout << "kusoge" << endl;
+    else cout << a << endl;
+    return 0;
+}
