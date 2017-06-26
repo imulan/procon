cac3f90ae26a139e55c9ceb8b2506f3222162645
imulan <k0223.teru@gmail.com>
2017-06-26 22:31:48 +0900

    Add SubtreeSumHash

---
+++ topcoder/SRM/TCO 2017 Round 1B/SubtreeSumHash.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+const ll mod=1e9+7;
+
+class SubtreeSumHash {
+    public:
+    ll mod_pow(ll x, ll n)
+    {
+        ll ret = 1;
+        while(n)
+        {
+            if(n&1) (ret*=x)%=mod;
+            (x*=x)%=mod;
+            n>>=1;
+        }
+        return ret;
+    }
+
+    int count(vector<int> weight, vector<int> p, int x) {
+        int n = weight.size();
+        vector<vector<int>> ch(n);
+
+        rep(i,p.size()) ch[p[i]].pb(i+1);
+
+        vector<ll> dp(n);
+        rep(i,n) dp[i] = mod_pow(x,weight[i]);
+
+        for(int i=n-1; i>=0; --i)
+        {
+            ll mul=1;
+            for(const auto &idx:ch[i]) (mul*=(dp[idx]+1))%=mod;
+            (dp[i]*=mul)%=mod;
+        }
+
+        ll ans = 0;
+        rep(i,n) (ans+=dp[i])%=mod;
+        return (int)ans;
+    }
+};
+
+// CUT begin
+ifstream data("SubtreeSumHash.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> weight, vector<int> p, int x, int __expected) {
+    time_t startClock = clock();
+    SubtreeSumHash *instance = new SubtreeSumHash();
+    int __result = instance->count(weight, p, x);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> weight;
+        from_stream(weight);
+        vector<int> p;
+        from_stream(p);
+        int x;
+        from_stream(x);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(weight, p, x, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1498482825;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "SubtreeSumHash (1000 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
