35ab0b37849235594fe70ed80bf835db1a17055b
imulan <k0223.teru@gmail.com>
2017-07-13 03:27:44 +0900

    Solve 2614

---
+++ aoj/vol26/2614.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+struct RollingHash{
+    static const int MD = 3;
+    static const vector<ll> hash_base, hash_mod;
+
+    int n;
+    vector<ll> hs[MD], pw[MD];
+
+    RollingHash(){}
+    RollingHash(const string &s){
+        n = s.size();
+        rep(i,MD){
+            hs[i].assign(n+1,0);
+            pw[i].assign(n+1,0);
+            hs[i][0] = 0;
+            pw[i][0] = 1;
+            rep(j,n){
+                pw[i][j+1] = pw[i][j]*hash_base[i] % hash_mod[i];
+                hs[i][j+1] = (hs[i][j]*hash_base[i]+s[j]) % hash_mod[i];
+            }
+        }
+    }
+
+    // 1-index
+    ll hash_value(int l, int r, int i){
+        return ((hs[i][r] - hs[i][l]*pw[i][r-l])%hash_mod[i]+hash_mod[i])%hash_mod[i];
+    }
+
+    bool match(int l1, int r1, int l2, int r2){
+        bool ret = true;
+        rep(i,MD) ret &= (hash_value(l1-1,r1,i) == hash_value(l2-1,r2,i));
+        return ret;
+    }
+
+    vector<ll> calc(int l, int r){
+        vector<ll> ret(MD);
+        rep(i,MD) ret[i]=hash_value(l-1,r,i);
+        return ret;
+    }
+};
+const vector<ll> RollingHash::hash_base{1009,1021,1013};
+const vector<ll> RollingHash::hash_mod{1000000009,1000000007,1000000021};
+
+int main()
+{
+    string s,t;
+    cin >>s >>t;
+    RollingHash sh(s), th(t);
+
+    int S = s.size(), T = t.size();
+    int ans = 0;
+    rep(i,S-T+1)
+    {
+        int l=0,r=T+1;
+        while(r-l>1)
+        {
+            int m =(l+r)/2;
+            if(sh.calc(1+i,1+i+m-1) == th.calc(1,m)) l=m;
+            else r=m;
+        }
+
+        if(l>=T-1)
+        {
+            ans += (l==T-1);
+            continue;
+        }
+
+        ans += (sh.calc(1+i+l+1,i+T) == th.calc(l+2,T));
+    }
+    cout << ans << endl;
+    return 0;
+}
