b6c5a0eaf1fbd927bfaf2068b7cf57301aae6a3c
imulan <k0223.teru@gmail.com>
2017-08-18 04:40:11 +0900

    Solve 556

---
+++ yukicoder/556.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+struct UF{
+    int n;
+    //正だったらその頂点の親,負だったら根で連結成分の個数
+    vector<int> d;
+    UF() {}
+    UF(int N):n(N), d(N,-1){}
+    int root(int v){
+        if(d[v]<0) return v;
+        return d[v]=root(d[v]);
+    }
+    bool unite(int X,int Y){
+        X=root(X); Y=root(Y);
+        if(X==Y) return false;
+        if(size(X)<size(Y) || (size(X)==size(Y)&&(X>Y))) swap(X,Y);
+        d[X]+=d[Y];
+        d[Y]=X;
+        return true;
+    }
+    int size(int v){ return -d[root(v)]; }
+    bool same(int X,int Y){ return root(X)==root(Y); }
+};
+
+int main()
+{
+    int n,m;
+    cin >>n >>m;
+    UF uf(n);
+    rep(i,m)
+    {
+        int a,b;
+        cin >>a >>b;
+        --a;
+        --b;
+        if(!uf.same(a,b)) uf.unite(a,b);
+    }
+
+    rep(i,n) cout << uf.root(i)+1 << endl;
+    return 0;
+}
