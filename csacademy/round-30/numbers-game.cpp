f0e4de57f4b9e702c58a410f95754bbd136dfee4
imulan <k0223.teru@gmail.com>
2017-09-01 02:22:01 +0900

    Solve numbers-game

---
+++ csacademy/round-30/numbers-game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    int n;
+    cin >>n;
+    vector<string> s(n);
+    rep(i,n) cin >>s[i];
+
+    int ans = 0;
+    rep(i,n-1) ans += (s[i][1]!=s[i+1][0]);
+    cout << ans << endl;
+    return 0;
+}
