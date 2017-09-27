e38ca29bf647a69517062f4c44d9a2f0107888d9
imulan <k0223.teru@gmail.com>
2017-09-27 13:53:27 +0900

    Solve 620D

--- codeforces/620D.cpp
+++ codeforces/620D.cpp
@@ -1,48 +1,81 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <map>
-#include <algorithm>
-#include <set>
-#include <sstream>
-#include <utility>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <cctype>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
 
-typedef long long ll;
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n) for(int i=0;i<(n);++i)
-#define foreach(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); itr++)
+using pi = pair<int,int>;
+using P = pair<ll,pi>;
 
-ll ab(ll p){
-  return (p>0)?p:-p;
-}
+int main()
+{
+    int n;
+    cin >>n;
+    vector<ll> a(n);
+    rep(i,n) cin >>a[i];
 
-int main(int argc, char const *argv[]) {
-  int n,m;
-  ll sa=0, sb=0;
+    int m;
+    cin >>m;
+    vector<ll> b(m);
+    rep(i,m) cin >>b[i];
 
-  cin >>n;
-  std::vector<long> a(n);
-  REP(i,n){
-    scanf(" %ld", &a[i]);
-    sa+=a[i];
-  }
-  cin >>m;
-  std::vector<long> b(m);
-  REP(i,m){
-    scanf(" %ld", &b[i]);
-    sb+=b[i];
-  }
+    ll sa = accumulate(all(a),0LL), sb = accumulate(all(b),0LL);
 
+    vector<pi> ans;
+    ll val = abs(sa - sb);
 
+    vector<P> v;
+    rep(i,n)rep(j,m) v.pb(P(-a[i]+b[j],pi(i,j)));
+    sort(all(v));
+    int V = v.size();
 
-  return 0;
+    rep(i,n)rep(j,m)
+    {
+        sa = sa - a[i] + b[j];
+        sb = sb - b[j] + a[i];
+
+        if(abs(sa-sb)<val)
+        {
+            val = abs(sa-sb);
+            ans.resize(0);
+            ans.pb({i,j});
+        }
+
+        ll d = sa-sb;
+        int idx = lower_bound(all(v),P(-d/2,pi(-1,-1)))-v.begin();
+        for(int k=-1; k<=1; ++k)
+        {
+            int vv = idx+k;
+            if(0<=vv && vv<V && v[vv].se.fi!=i && v[vv].se.se!=j)
+            {
+                sa += v[vv].fi;
+                sb -= v[vv].fi;
+
+                if(abs(sa-sb)<val)
+                {
+                    val = abs(sa-sb);
+                    ans.resize(0);
+                    ans.pb({i,j});
+                    ans.pb(v[vv].se);
+                }
+
+                sa -= v[vv].fi;
+                sb += v[vv].fi;
+            }
+        }
+
+        sa = sa + a[i] - b[j];
+        sb = sb + b[j] - a[i];
+    }
+
+    cout << val << endl;
+    cout << ans.size() << endl;
+    rep(i,ans.size()) cout << ans[i].fi+1 << " " << ans[i].se+1 << endl;
+    return 0;
 }
