c1036a5bf08a225c251086b30d3881712a8baed0
imulan <k0223.teru@gmail.com>
2017-10-06 04:31:30 +0900

    Solve 0345

---
+++ aoj/vol3/0345.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    vector<int> e(4);
+    rep(i,4) cin >>e[i];
+    sort(all(e));
+
+    string ans = "yes";
+    rep(i,2)if(e[2*i]!=e[2*i+1]) ans = "no";
+    cout << ans << endl;
+    return 0;
+}
