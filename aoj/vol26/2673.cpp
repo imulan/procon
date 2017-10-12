f08f497ab2fb8fb2dd099c72ead33da7454e9a33
imulan <k0223.teru@gmail.com>
2017-10-13 08:07:28 +0900

    Solve 2673

---
+++ aoj/vol26/2673.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+/* 基本要素 */
+typedef complex<double> Point;
+typedef pair<Point, Point> Line;
+typedef vector<Point> VP;
+const double EPS = 1e-6; // 許容誤差^2
+const double INF = 1e9;
+#define X real()
+#define Y imag()
+// #define LE(n,m) ((n) < (m) + EPS)
+#define LE(n,m) ((n) - (m) < EPS)
+// #define GE(n,m) ((n) + EPS > (m))
+#define GE(n,m) (EPS > (m) - (n))
+#define EQ(n,m) (abs((n)-(m)) < EPS)
+
+// 内積　dot(a,b) = |a||b|cosθ
+double dot(Point a, Point b) {
+  return a.X*b.X + a.Y*b.Y;
+}
+
+// 外積　cross(a,b) = |a||b|sinθ
+double cross(Point a, Point b) {
+  return a.X*b.Y - a.Y*b.X;
+}
+
+// 点の進行方向
+int ccw(Point a, Point b, Point c) {
+  b -= a;  c -= a;
+  if (cross(b,c) >  EPS) return +1;  // counter clockwise
+  if (cross(b,c) < -EPS) return -1;  // clockwise
+  if (dot(b,c)   < -EPS) return +2;  // c--a--b on line
+  if (norm(b) < norm(c)) return -2;  // a--b--c on line or a==b
+  return 0;                          // a--c--b on line or a==c or b==c
+}
+
+// 直線と線分
+bool isecLS(Point a1, Point a2, Point b1, Point b2) {
+  return cross(a2-a1, b1-a1) * cross(a2-a1, b2-a1) < EPS;
+}
+
+// 2直線の交点
+Point crosspointLL(Point a1, Point a2, Point b1, Point b2) {
+  double d1 = cross(b2-b1, b1-a1);
+  double d2 = cross(b2-b1, a2-a1);
+  if (EQ(d1, 0) && EQ(d2, 0)) return a1;  // same line
+  assert(!EQ(d2, 0)); // 交点がない
+  return a1 + d1/d2 * (a2-a1);
+}
+
+// 多角形の符号付面積
+double area(const VP& ps) {
+  double a = 0;
+  rep (i, ps.size()) a += cross(ps[i], ps[(i+1) % ps.size()]);
+  return a / 2;
+}
+
+using pd = pair<double,double>;
+
+int main()
+{
+    int n;
+    cin >>n;
+    vector<int> x(n),y(n);
+    rep(i,n) cin >>x[i] >>y[i];
+
+    int minx = 100000;
+    rep(i,n) minx = min(minx,x[i]);
+
+    rep(i,n)
+    {
+        x[i] -= minx;
+        x[i] *= 2;
+    }
+
+    VP p(n);
+    rep(i,n) p[i] = Point(x[i],y[i]);
+
+    double whole = abs(area(p));
+    double s = 0;
+    for(int i=0; i<=4002; i+=2)
+    {
+        vector<pd> seg;
+        rep(j,n)if(x[j]!=x[(j+1)%n] && isecLS(Point(i,-1001),Point(i,1001),p[j],p[(j+1)%n]) && isecLS(Point(i+1,-1001),Point(i+1,1001),p[j],p[(j+1)%n]))
+        {
+            seg.pb({crosspointLL(p[j], p[(j+1)%n], Point(i,-1001), Point(i,1001)).Y, crosspointLL(p[j], p[(j+1)%n], Point(i+1,-1001), Point(i+1,1001)).Y});
+        }
+
+        sort(all(seg));
+        reverse(all(seg));
+        int SZ = seg.size();
+        assert(SZ%2==0);
+        for(int j=0; j<SZ; j+=2)
+        {
+            double a = seg[j].fi - seg[j+1].fi;
+            double b = seg[j].se - seg[j+1].se;
+            s += (a+b)/2;
+        }
+
+    }
+
+    printf("%.10f\n", max(s,whole-s)/2);
+    return 0;
+}
