123312f5591019526ecbe070a21ae43f1eb27bfe
imulan <k0223.teru@gmail.com>
2017-10-18 03:57:26 +0900

    Solve 782D

---
+++ codeforces/782D.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+struct SCC{
+    int V;
+    vector<vector<int>> G, rG;
+    vector<int> vs; // 帰りがけ順の並び
+    vector<int> cmp; //属する強連結成分トポロジカル順序
+    vector<bool> used;
+
+    SCC(){}
+    SCC(int n){
+        V = n;
+        G = vector<vector<int>>(n);
+        rG = vector<vector<int>>(n);
+    }
+
+    void add_edge(int from, int to){
+        G[from].push_back(to);
+        rG[to].push_back(from);
+    }
+
+    void dfs(int v){
+        used[v] = true;
+        rep(i,G[v].size())if(!used[G[v][i]]) dfs(G[v][i]);
+        vs.push_back(v);
+    }
+
+    void rdfs(int v, int k){
+        used[v]=true;
+        cmp[v]=k;
+        rep(i,rG[v].size())if(!used[rG[v][i]]) rdfs(rG[v][i],k);
+    }
+
+    int scc(){
+        used = vector<bool>(V,false);
+        vs.clear();
+        rep(i,V)if(!used[i]) dfs(i);
+
+        used = vector<bool>(V,false);
+        cmp = vector<int>(V);
+        int num_scc = 0;
+        for(int i=vs.size()-1; i>=0; --i)if(!used[vs[i]]) rdfs(vs[i],num_scc++);
+        return num_scc;
+    }
+};
+
+struct TwoSat{
+    int v;
+    SCC graph;
+
+    // v literals
+    // 0~v-1: true
+    // v~2v-1: false
+
+    TwoSat(int num_literal){
+        v = num_literal;
+        graph = SCC(2*v);
+    }
+
+    inline int num(int id, bool b){return id+(b?0:v);}
+
+    void add_clause(int x, bool X, int y, bool Y){
+        graph.add_edge(num(x,!X), num(y,Y));
+        graph.add_edge(num(y,!Y), num(x,X));
+    }
+
+    // 割り当てが可能か調べる
+    bool calc(){
+        graph.scc();
+        rep(i,v)if(graph.cmp[i]==graph.cmp[v+i]) return false;
+        return true;
+    }
+
+    // リテラルの真偽値を返す
+    vector<bool> get_literals(){
+        assert(calc());
+        vector<bool> res(v);
+        rep(i,v) res[i] = (graph.cmp[i]>graph.cmp[v+i]);
+        return res;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >>n;
+    vector<vector<string>> s(n);
+    rep(i,n)
+    {
+        string x,y;
+        cin >>x >>y;
+        s[i].pb(x.substr(0,3));
+        s[i].pb(x.substr(0,2)+y.substr(0,1));
+    }
+
+    TwoSat solver(n);
+    rep(i,n)rep(j,i)
+    {
+        rep(ii,2)rep(jj,2)
+        {
+            if(s[i][ii] == s[j][jj]) solver.add_clause(i,ii,j,jj);
+        }
+
+        if(s[i][0]==s[j][0])
+        {
+            solver.add_clause(i,true,j,false);
+            solver.add_clause(j,true,i,false);
+        }
+    }
+
+    string ans = "NO";
+    vector<string> v;
+    if(solver.calc())
+    {
+        ans = "YES";
+        vector<bool> literals = solver.get_literals();
+        rep(i,n)
+        {
+            int idx = 1;
+            if(literals[i]) idx = 0;
+            v.pb(s[i][idx]);
+        }
+    }
+
+    cout << ans << endl;
+    for(string w:v) cout << w << endl;
+    return 0;
+}
