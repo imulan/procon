0ad65b38a2a05db64ec00f43c85a59b6058d3730
imulan <k0223.teru@gmail.com>
2017-10-20 16:23:41 +0900

    Solve a

---
+++ atcoder/indeednow-finala-open/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    int n;
+    cin >>n;
+    vector<int> a(n);
+    rep(i,n) cin >>a[i];
+    sort(all(a));
+
+    int MIN = 191919, MAX = 0;
+    rep(i,n/2)
+    {
+        int s = a[i]+a[n-1-i];
+        MIN = min(MIN,s);
+        MAX = max(MAX,s);
+    }
+
+    printf("%d\n", MAX-MIN);
+    return 0;
+}
