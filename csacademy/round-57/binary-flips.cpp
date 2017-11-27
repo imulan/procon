6ba26dc1c07c0e85853c8fa54fff68f32377f7d4
imulan <k0223.teru@gmail.com>
2017-11-27 18:54:03 +0900

    Update binary-flips

--- csacademy/round-57/binary-flips.cpp
+++ csacademy/round-57/binary-flips.cpp
@@ -10,29 +10,32 @@ using ll = long long;
 template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
 template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
 
-using vl = vector<ll>;
 const ll mod = 1e9+7;
 
-vector<vl> make_table(int n, int k)
+vector<ll> make_table(int n, int k)
 {
-    vector<vl> dp(k+1,vl(n+1));
-    dp[0][0] = 1;
-    rep(i,k)rep(j,n+1)
+    vector<ll> dp(n+1);
+    dp[0] = 1;
+    rep(i,k)
     {
-        if(j+1<=n) (dp[i+1][j+1]+=dp[i][j]*(n-j))%=mod;
-        if(j-1>=0) (dp[i+1][j-1]+=dp[i][j]*j)%=mod;
+        vector<ll> nx(n+1);
+        rep(j,n+1)
+        {
+            if(j+1<=n) (nx[j+1]+=dp[j]*(n-j))%=mod;
+            if(j-1>=0) (nx[j-1]+=dp[j]*j)%=mod;
+        }
+        dp = nx;
     }
     return dp;
 }
 
 ll solve(int n, int m, int k, int s)
 {
-    vector<vl> dr = make_table(n,k), dc = make_table(m,k);
-
+    vector<ll> dr = make_table(n,k), dc = make_table(m,k);
     ll ans = 0;
     rep(r1,n+1)rep(c1,m+1)
     {
-        if(r1*(m-c1) + c1*(n-r1) == s) (ans += (dr[k][r1]*dc[k][c1])%mod) %= mod;
+        if(r1*(m-c1) + c1*(n-r1) == s) (ans += dr[r1]*dc[c1])%=mod;
     }
     return ans;
 }
