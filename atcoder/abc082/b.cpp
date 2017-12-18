558942fd0389f73c1efb1651f885ff5d56fd74e6
imulan <k0223.teru@gmail.com>
2017-12-19 04:27:22 +0900

    Solve b

---
+++ atcoder/abc082/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    string s,t;
+    cin >>s >>t;
+
+    sort(all(s));
+    sort(all(t));
+    reverse(all(t));
+    cout << (s<t?"Yes":"No") << endl;
+    return 0;
+}
