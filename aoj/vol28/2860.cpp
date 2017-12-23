2749ce307170f511fe4a3edd9b0e7f34dc86837e
imulan <k0223.teru@gmail.com>
2017-12-24 04:15:48 +0900

    refactor 2860

--- aoj/vol28/2860.cpp
+++ aoj/vol28/2860.cpp
@@ -14,15 +14,15 @@ const int N = 100000;
 vector<int> G[N];
 int n,k;
 
-int ans = 0;
-int sz[N];
-int dp[N],ed[N];
-void dfs(int x, int p)
+int sz[N],dp[N],ed[N];
+int dfs(int x, int p)
 {
+    int ret = 0;
+
     sz[x] = 1;
     for(int nx:G[x])if(nx!=p)
     {
-        dfs(nx,x);
+        ret = max(ret,dfs(nx,x));
         sz[x] += sz[nx];
     }
     if(sz[x]==1) dp[x] = -1;
@@ -30,42 +30,41 @@ void dfs(int x, int p)
     int ct = 0;
     for(int nx:G[x])if(nx!=p)
     {
-        ed[x] = max(ed[x],ed[nx]);
         dp[x] = max(dp[x],dp[nx]);
+        ed[x] = max(ed[x],ed[nx]);
         ct += (sz[nx]>=k);
     }
     ed[x] = max(ed[x],ct);
 
-    vector<int> gain;
+    int g0=-1, g1=-1;
     for(int nx:G[x])if(nx!=p)
     {
         int tmp = ct - (sz[nx]>=k);
         dp[x] = max(dp[x],ed[nx]+tmp);
         ed[x] = max(ed[x],ed[nx]+tmp);
 
-        gain.pb(ed[nx]-(sz[nx]>=k));
+        int tg = ed[nx]-(sz[nx]>=k);
+        if(g1<tg) g1=tg;
+        if(g0<g1) swap(g0,g1);
     }
+    dp[x] = max(dp[x],ct+g0+g1);
 
-    sort(all(gain),greater<int>());
-    if(gain.size()>=2) dp[x] = max(dp[x],ct+gain[0]+gain[1]);
-
-    ans = max(ans,dp[x]+(n-sz[x]>=k));
-    // printf(" %d: dp %d ed %d\n", x,dp[x],ed[x]);
+    ret = max(ret,dp[x]+(n-sz[x]>=k));
+    return ret;
 }
 
 int main()
 {
-    cin >>n >>k;
+    scanf(" %d %d", &n, &k);
     rep(i,n-1)
     {
         int u,v;
-        cin >>u >>v;
+        scanf(" %d %d", &u, &v);
         --u; --v;
         G[u].pb(v);
         G[v].pb(u);
     }
 
-    dfs(0,-1);
-    cout << ans << endl;
+    printf("%d\n", dfs(0,-1));
     return 0;
 }
