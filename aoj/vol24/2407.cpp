a9ebeeea6b5279da7f254cd4ba94fc7ec127a278
imulan <k0223.teru@gmail.com>
2018-01-01 23:58:33 +0900

    Solve 2407

---
+++ aoj/vol24/2407.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main()
+{
+    string s;
+    cin >>s;
+    int S = s.size();
+
+    int ct = 0;
+    rep(i,S-1) ct+=(s[i]!=s[i+1]);
+
+    cout << (ct%2?'o':s[0]) << endl;
+    return 0;
+}
