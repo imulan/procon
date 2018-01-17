b8d1d8d2bacd815765717ebe7e5f00d457c70d7b
imulan <k0223.teru@gmail.com>
2018-01-17 14:31:30 +0900

    Update StringSplit.cpp

--- lib/StringSplit.cpp
+++ lib/StringSplit.cpp
@@ -1,6 +1,6 @@
 vector<string> split(const string &str, char sep){
     vector<string> v;
-    stringstream ss(str);
+    stringstream ss(str + sep);
     string buffer;
     while( getline(ss,buffer,sep ) ) v.pb(buffer);
     return v;
