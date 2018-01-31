c424f4f6b050bea025f256926811ed57c09eceb1
imulan <k0223.teru@gmail.com>
2018-01-31 15:04:04 +0900

    Solve 900B

---
+++ codeforces/900B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+const int LIM = 19191919;
+
+int main(){
+    int a,b,c;
+    cin >>a >>b >>c;
+
+    a %= b;
+    int ans = -1;
+    for(int i=1; i<LIM; ++i){
+        a *= 10;
+        int d = a/b;
+        if(c==d){
+            ans = i;
+            break;
+        }
+        a %= b;
+    }
+    cout << ans << endl;
+    return 0;
+}
