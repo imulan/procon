04167ad152376da0fa36d9dd99ebf4e7a1e5c287
imulan <k0223.teru@gmail.com>
2018-03-15 02:20:47 +0900

    init HL_Decomposition.cpp

---
+++ lib/HL_Decomposition.cpp
@@ -0,0 +1,70 @@
+struct HL_decomposition{
+    int n;
+    vector<vector<int>> G;
+    vector<int> vid, inv, depth, par, heavy, head, sub;
+    /*
+    vid : HL分解後のグラフでの頂点ID
+    inv : HL分解前のグラフでのvid[i]の頂点ID
+    depth : rootからの距離
+    par : 根付き木上での親
+    heavy : heavy-path上における頂点iの次の頂点ID
+    head : 頂点iが属するheavy-pathの先頭の頂点ID
+    sub : 部分木のサイズ
+    */
+
+    HL_decomposition(){}
+    HL_decomposition(int sz) : n(sz), G(n), vid(n), inv(n), depth(n), par(n), heavy(n,-1), head(n), sub(n) {}
+
+    void add_edge(int u, int v){
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+
+    void build(int root = 0){
+        dfs(root, -1);
+        bfs(root);
+    }
+
+    void dfs(int cur, int pre){
+        par[cur] = pre;
+        sub[cur] = 1;
+        int max_sub = 0;
+        for(int nx:G[cur])if(nx != pre){
+            depth[nx] = depth[cur] + 1;
+            dfs(nx, cur);
+
+            sub[cur] += sub[nx];
+            if(max_sub < sub[nx]){
+                max_sub = sub[nx];
+                heavy[cur] = nx;
+            }
+        }
+    }
+
+    void bfs(int root){
+        int k = 0;
+        queue<int> que({root});
+        while(!que.empty()){
+            int cur = que.front();
+            que.pop();
+            // curを先頭とするheavy-pathを処理
+            for(int i=cur; i!=-1; i=heavy[i]){
+                vid[i] = k++;
+                inv[vid[i]] = i;
+                head[i] = cur;
+                // heavy-pathの先頭になるなら、queにpushする
+                for(int nx:G[i])if(nx != par[i] && nx != heavy[i]){
+                    que.push(nx);
+                }
+            }
+        }
+    }
+
+    int lca(int u, int v){
+        while(1){
+            if(vid[u] > vid[v]) swap(u,v);
+            if(head[u] == head[v]) return u;
+            v = par[head[v]];
+        }
+    }
+};
