3f32e32ccb64b46faebfd9b842241c2491256406
imulan <k0223.teru@gmail.com>
2018-03-17 20:42:47 +0900

    Solve 939D

---
+++ codeforces/939D.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+struct UF{
+    int n;
+    //正だったらその頂点の親,負だったら根で連結成分の個数
+    vector<int> d;
+    UF() {}
+    UF(int N):n(N), d(N,-1){}
+    int root(int v){
+        if(d[v]<0) return v;
+        return d[v]=root(d[v]);
+    }
+    bool unite(int X,int Y){
+        X=root(X); Y=root(Y);
+        if(X==Y) return false;
+        if(size(X) < size(Y)) swap(X,Y);
+        d[X]+=d[Y];
+        d[Y]=X;
+        return true;
+    }
+    int size(int v){ return -d[root(v)]; }
+    bool same(int X,int Y){ return root(X)==root(Y); }
+};
+
+const int A = 26;
+
+int main(){
+    int n;
+    string s,t;
+    cin >>n >>s >>t;
+
+    vector<pair<char,char>> ans;
+    UF uf(A);
+    rep(i,n){
+        int u = s[i]-'a', v = t[i]-'a';
+
+        if(!uf.same(u,v)){
+            uf.unite(u,v);
+            ans.pb({s[i],t[i]});
+        }
+    }
+
+    cout << ans.size() << endl;
+    for(const auto &p:ans) cout << p.fi << " " << p.se << endl;
+    return 0;
+}
