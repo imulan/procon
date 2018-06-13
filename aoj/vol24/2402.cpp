fc7f334ca51fcbb916b61ab831506a1067745b92
imulan <k0223.teru@gmail.com>
2018-06-13 16:45:32 +0900

    Solve 2402

---
+++ aoj/vol24/2402.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+/* 基本要素 */
+typedef complex<double> Point;
+typedef pair<Point, Point> Line;
+typedef vector<Point> VP;
+const double EPS = 1e-7; // 許容誤差^2
+const double INF = 1e9;
+#define X real()
+#define Y imag()
+// #define LE(n,m) ((n) < (m) + EPS)
+#define LE(n,m) ((n) - (m) < EPS)
+// #define GE(n,m) ((n) + EPS > (m))
+#define GE(n,m) (EPS > (m) - (n))
+#define EQ(n,m) (abs((n)-(m)) < EPS)
+
+// 内積　dot(a,b) = |a||b|cosθ
+double dot(Point a, Point b) {
+  return a.X*b.X + a.Y*b.Y;
+}
+
+// 外積　cross(a,b) = |a||b|sinθ
+double cross(Point a, Point b) {
+  return a.X*b.Y - a.Y*b.X;
+}
+
+// 点の進行方向
+int ccw(Point a, Point b, Point c) {
+  b -= a;  c -= a;
+  if (cross(b,c) >  EPS) return +1;  // counter clockwise
+  if (cross(b,c) < -EPS) return -1;  // clockwise
+  if (dot(b,c)   < -EPS) return +2;  // c--a--b on line
+  if (norm(b) < norm(c)) return -2;  // a--b--c on line or a==b
+  return 0;                          // a--c--b on line or a==c or b==c
+}
+
+// 線分と線分
+bool isecSS(Point a1, Point a2, Point b1, Point b2) {
+  return ccw(a1, a2, b1)*ccw(a1, a2, b2) <= 0 &&
+  ccw(b1, b2, a1)*ccw(b1, b2, a2) <= 0;
+}
+
+// 線分と点
+bool isecSP(Point a1, Point a2, Point b) {
+  return !ccw(a1, a2, b);
+}
+
+// 点pの直線aへの射影点を返す
+Point proj(Point a1, Point a2, Point p) {
+  return a1 + dot(a2-a1, p-a1)/norm(a2-a1) * (a2-a1);
+}
+
+double distSP(Point a1, Point a2, Point p) {
+  Point r = proj(a1, a2, p);
+  if (isecSP(a1, a2, r)) return abs(r-p);
+  //= !ccw(a1, a2, r)
+  return min(abs(a1-p), abs(a2-p));
+}
+
+double distSS(Point a1, Point a2, Point b1, Point b2) {
+  if (isecSS(a1, a2, b1, b2)) return 0;
+  return min(min(distSP(a1, a2, b1), distSP(a1, a2, b2)),
+   min(distSP(b1, b2, a1), distSP(b1, b2, a2)));
+}
+
+const double pi = acos(-1);
+
+int main(){
+    int n,s,t;
+    while(cin >>n >>s >>t,n){
+
+        vector<VP> star(n);
+        rep(i,n){
+            double x,y,a,r;
+            cin >>x >>y >>a >>r;
+
+            Point c(x,y);
+
+            star[i] = VP(5);
+            rep(j,5){
+                star[i][j] = c + polar(r,pi/2 + (a+72*2*j)*2*pi/360);
+            }
+        }
+
+        auto dist_star = [&](VP a, VP b){
+            double ret = INF;
+            rep(i,5)rep(j,5){
+                ret = min(ret, distSS(a[i],a[(i+1)%5],b[j],b[(j+1)%5]));
+            }
+            return ret;
+        };
+
+        vector<vector<double>> d(n,vector<double>(n,INF));
+        rep(i,n) d[i][i] = 0;
+        rep(i,n)rep(j,i){
+            d[i][j] = d[j][i]= dist_star(star[i],star[j]);
+        }
+
+        rep(k,n)rep(i,n)rep(j,n) d[i][j] = min(d[i][j], d[i][k]+d[k][j]);
+        printf("%.10f\n", d[s-1][t-1]);
+    }
+    return 0;
+}
