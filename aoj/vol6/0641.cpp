942c6895d80e71f83169437d3f90b4f02631dc54
imulan <k0223.teru@gmail.com>
2018-07-11 20:33:14 +0900

    Solve 0641

---
+++ aoj/vol6/0641.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main(){
+    int n,a,b,c,d;
+    cin >>n >>a >>b >>c >>d;
+
+    int x = (n+a-1)/a, y = (n+c-1)/c;
+    cout << min(b*x, d*y) << endl;
+    return 0;
+}
