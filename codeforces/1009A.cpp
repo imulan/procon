77e62ee0f65cca4c2e968754e20a38e21d2b8c4c
imulan <k0223.teru@gmail.com>
2018-07-18 15:30:38 +0900

    Solve 1009A

---
+++ codeforces/1009A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main(){
+    int n,m;
+    cin >>n >>m;
+    vector<int> c(n),a(m);
+    rep(i,n) cin >>c[i];
+    rep(i,m) cin >>a[i];
+
+    int idx = 0;
+    int ans = 0;
+    rep(i,n){
+        if(idx<m && c[i]<=a[idx]){
+            ++idx;
+            ++ans;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
