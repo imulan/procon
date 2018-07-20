81983774a8e91bc7d7ce9ea6a1702764919a7c62
imulan <k0223.teru@gmail.com>
2018-07-20 22:19:33 +0900

    Solve 3018

---
+++ aoj/vol30/3018.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main(){
+    ll n;
+    cin >>n;
+    int x=0;
+    while(n>>x&1) ++x;
+    cout << (1LL<<x) << endl;
+    return 0;
+}
