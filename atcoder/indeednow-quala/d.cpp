9d37d813007a0b995aeed5d8d4e9b2c5f3a73623
imulan <k0223.teru@gmail.com>
2018-08-10 18:47:01 +0900

    Solve d

--- atcoder/indeednow-quala/d.cpp
+++ atcoder/indeednow-quala/d.cpp
@@ -1,44 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
+using ll = long long;
 #define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
-#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
 #define all(x) (x).begin(),(x).end()
-#define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
 
-inline int ab(int x)
-{
-	return (x>0)?x:-x;
+using pi = pair<int,int>;
+using vi = vector<int>;
+using mat = vector<vi>;
+
+const int dx[4]={1,-1,0,0};
+const int dy[4]={0,0,1,-1};
+
+int h,w;
+
+inline bool IN(int y, int x){
+    return 0<=y && y<h && 0<=x && x<w;
 }
 
-int main()
-{
-	int h,w;
-	int c[6][6];
+map<mat,int> dp;
 
-	cin >>h >>w;
-	rep(i,h)rep(j,w) cin >>c[i][j];
+int ans = 24;
+mat now;
+pi pos;
+void dfs(int d){
+    if(dp.count(now)) ans = min(ans, d+dp[now]);
+    if(d==12) return;
 
-	int ans=0;
+    pi def = pos;
+    rep(i,4){
+        int ny = pos.fi+dy[i], nx = pos.se+dx[i];
+        if(IN(ny,nx)){
+            swap(now[ny][nx], now[pos.fi][pos.se]);
+            pos = {ny,nx};
+            dfs(d+1);
+            pos = def;
+            swap(now[ny][nx], now[pos.fi][pos.se]);
+        }
+    }
+}
 
-	for(int i=1; i<h*w; ++i)
-	{
-		int f=(i-1)/w, s=(i-1)%w;
+int main(){
+    cin >>h >>w;
 
-		rep(y,h)rep(x,w)
-		{
-			if(c[y][x]==i)
-			{
-				ans+=ab(y-f)+ab(x-s);
-				break;
-			}
-		}
-	}
+    mat s(h,vi(w));
+    rep(i,h)rep(j,w) cin >>s[i][j];
+    pi spos;
+    rep(i,h)rep(j,w)if(s[i][j]==0) spos = {i,j};
 
-	std::cout << ans << std::endl;
-	return 0;
+    using state = pair<mat,pi>;
+    queue<state> que;
+    dp[s] = 0;
+    que.push({s,spos});
+    while(!que.empty()){
+        state cur = que.front();
+        que.pop();
+
+        mat c = cur.fi;
+        pi cpos = cur.se;
+
+        int D = dp[c];
+        if(D==11) continue;
+
+        rep(i,4){
+            int ny = cpos.fi+dy[i], nx = cpos.se+dx[i];
+            if(IN(ny,nx)){
+                swap(c[ny][nx], c[cpos.fi][cpos.se]);
+                if(!dp.count(c)){
+                    dp[c] = D+1;
+                    que.push({c, {ny,nx}});
+                }
+                swap(c[ny][nx], c[cpos.fi][cpos.se]);
+            }
+        }
+    }
+
+    now = mat(h,vi(w));
+    rep(i,h)rep(j,w) now[i][j] = i*w+j+1;
+    now[h-1][w-1] = 0;
+    pos = {h-1,w-1};
+
+    dfs(0);
+
+    cout << ans << endl;
+    return 0;
 }
