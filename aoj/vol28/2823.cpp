11f514ca2f492e02f537748ed87d6a8fa16787cf
imulan <k0223.teru@gmail.com>
2018-09-27 02:19:13 +0900

    Solve 2823

---
+++ aoj/vol28/2823.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main(){
+    int n,m;
+    while(cin >>n >>m,n){
+        vector<int> a(m);
+        rep(i,n){
+            int d,v;
+            cin >>d >>v;
+            --d;
+            a[d] = max(a[d], v);
+        }
+
+        int ans = 0;
+        rep(i,m) ans += a[i];
+        cout << ans << endl;
+    }
+    return 0;
+}
