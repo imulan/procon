baf44acd4d150564c123ebb8b2f9864702200e7c
imulan <k0223.teru@gmail.com>
2018-10-04 19:33:18 +0900

    Add rank()

--- lib/gauss_jordan.cpp
+++ lib/gauss_jordan.cpp
@@ -36,3 +36,28 @@ vec gauss_jordan(const mat &A, const vec &b){
     rep(i,d) ret[i] = B[i][d];
     return ret;
 }
+
+// 行列の階数を求める
+// 係数行列と拡大係数行列の階数が異なるときは不能
+// 係数行列と拡大係数行列の階数が等しく、係数行列の階数と変数の個数が異なるときは不定
+// 係数行列と拡大係数行列の階数と変数の個数がすべて等しいときは一意解が存在
+int rank(mat A){
+    const int R = A.size(), C = A[0].size();
+    int ret = 0;
+    for(int i=0; ret<R&&i<C; ++i){
+        int pivot = ret;
+        for(int j=ret+1; j<R; ++j){
+            if(abs(A[j][i]) > abs(A[pivot][i])) pivot = j;
+        }
+        swap(A[pivot], A[ret]);
+
+        if(abs(A[ret][i]) < EPS) continue;
+
+        for(int k=C-1; k>=i; --k) A[ret][k] /= A[ret][i];
+        for(int j=ret+1; j<R; ++j){
+            for(int k=i; k<C; ++k) A[j][k] -= A[ret][k]*A[j][i];
+        }
+        ++ret;
+    }
+    return ret;
+}
