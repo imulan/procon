2ee8b1fcd960610982c08d3c353b861f1ec8f93f
imulan <k0223.teru@gmail.com>
2018-11-26 20:44:54 +0900

    Solve 1077A

---
+++ codeforces/1077A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+
+int main(){
+    int t;
+    cin >>t;
+    rep(i,t){
+        ll a,b,k;
+        cin >>a >>b >>k;
+
+        ll x = (a-b)*(k/2);
+        if(k%2==1) x += a;
+        cout << x << endl;
+    }
+    return 0;
+}
