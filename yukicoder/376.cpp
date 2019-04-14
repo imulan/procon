d86807e444c7aae3c49dc1544adb84dd91e073e3
imulan <k0223.teru@gmail.com>
2019-04-15 00:35:41 +0900

    Update 376

--- yukicoder/376.cpp
+++ yukicoder/376.cpp
@@ -1,76 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
+using ll = long long;
 #define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
-#define each(itr,c) for(__typeof(c.begin()) itr=c.begin(); itr!=c.end(); ++itr)
 #define all(x) (x).begin(),(x).end()
 #define pb push_back
 #define fi first
 #define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
 
-vector<ll> factorize(ll n)
-{
+const ll INF = LLONG_MAX/3;
+
+vector<ll> divisor(ll n){
     vector<ll> ret;
-
-    ll t=n;
-    for(ll i=2; i*i<=n; ++i)
-    {
-        while(t%i==0)
-        {
-            t/=i;
+    for(ll i=1; i*i<=n; ++i){
+        if(n%i == 0){
             ret.pb(i);
+            if(n/i != i) ret.pb(n/i);
         }
     }
-    if(t>1) ret.pb(t);
-    sort(all(ret),greater<ll>());
+    sort(all(ret));
     return ret;
 }
 
-const ll INF=1234567890123456LL;
-
-vector<ll> d;
-int D;
-vector<ll> m;
-
-typedef pair<ll,ll> pl;
-typedef pair<int,pl> P;
-map<P,ll> dp;
-
-ll dfs(int now, ll a, ll c)
-{
-    ll b = m[now]/a/c;
-
-    if(!(a<=b && b<=c)) return INF;
-    if(now==D) return a+b+c-3;
-
-    P state(now,pl(a,c));
-
-    if(dp.find(state) != dp.end()) return dp[state];
-
-    ll ret=INF;
-
-    ll na=a*d[now], nb=b*d[now], nc=c*d[now];
-    ret = min(ret, dfs(now+1, min({na,b,c}), max({na,b,c})));
-    ret = min(ret, dfs(now+1, min({a,nb,c}), max({a,nb,c})));
-    ret = min(ret, dfs(now+1, min({a,b,nc}), max({a,b,nc})));
-
-    dp[state]=ret;
-    return ret;
-}
-
-int main()
-{
+int main(){
     ll n;
     cin >>n;
 
-    d=factorize(n);
-    D = d.size();
+    vector<ll> d = divisor(n);
+    int D = d.size();
 
-    m = vector<ll>(D+1);
-    m[0]=1;
-    for(int i=1; i<=D; ++i) m[i]=m[i-1]*d[i-1];
+    ll ans = INF;
+    rep(i,D){
+        ll a = d[i];
 
-    cout << dfs(0,1,1) << " " << n-1 << endl;
+        ll x = n/a;
+        rep(j,i+1){
+            ll b = d[j];
+            if(x%b != 0) continue;
+            ll c = x/b;
+
+            ans = min(ans, a+b+c-3);
+        }
+    }
+
+    cout << ans << " " << n-1 << "\n";
     return 0;
 }
