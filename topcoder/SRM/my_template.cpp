cd5827542664f53d524e6bf3ab0b2b91ead23ed4
imulan <cp.imulan@gmail.com>
2019-06-16 18:33:01 +0900

    Update template.cpp

--- topcoder/SRM/my_template.cpp
+++ topcoder/SRM/my_template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
 #define all(x) (x).begin(),(x).end()
 #define pb push_back
 #define fi first
