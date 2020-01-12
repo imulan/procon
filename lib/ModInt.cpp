466d3218097cfb23e42860079077950d6afddc6f
imulan <cp.imulan@gmail.com>
2020-01-12 22:20:23 +0900

    modify

--- lib/ModInt.cpp
+++ lib/ModInt.cpp
@@ -3,41 +3,31 @@ struct ModInt {
   int x;
 
   ModInt() : x(0) {}
-
   ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
 
   ModInt &operator+=(const ModInt &p) {
     if((x += p.x) >= mod) x -= mod;
     return *this;
   }
-
   ModInt &operator-=(const ModInt &p) {
     if((x += mod - p.x) >= mod) x -= mod;
     return *this;
   }
-
   ModInt &operator*=(const ModInt &p) {
     x = (int) (1LL * x * p.x % mod);
     return *this;
   }
-
   ModInt &operator/=(const ModInt &p) {
     *this *= p.inverse();
     return *this;
   }
 
   ModInt operator-() const { return ModInt(-x); }
-
   ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
-
   ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
-
   ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
-
   ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
-
   bool operator==(const ModInt &p) const { return x == p.x; }
-
   bool operator!=(const ModInt &p) const { return x != p.x; }
 
   ModInt inverse() const {
@@ -74,4 +64,5 @@ struct ModInt {
   static int get_mod() { return mod; }
 };
 
+const int mod = 1e9+7;
 using mint = ModInt< mod >;
