fb635f9d8ce18ce11134fcb1b8d6eb65a4fd6af3
imulan <cp.imulan@gmail.com>
2020-07-11 18:45:08 +0900

    Add

---
+++ topcoder/SRM/TCO 2020 Round 2A/GridSpiral.cpp
@@ -0,0 +1,139 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+// clang-format on
+
+const ll N = 10000000;
+
+class GridSpiral {
+   public:
+    long long findCell(int D) {
+        if (D == 1 || D == 3 || D == 5 || D == 7) return 0;
+
+        for (ll k = 3; k <= N; k += 2) {
+            ll x = (k - 2) * (k - 2);
+
+            ll y = k * k - 1;
+            if (y - x == D) return x;
+
+            vector<ll> p(
+                {x, x + k - 2, x + k - 2 + k - 1, x + k - 2 + 2 * (k - 1)});
+
+            ll X = k * k + 1;
+            vector<ll> q({X, X + k, X + 2 * k + 1, X + 3 * k + 2});
+
+            rep(i, 4) {
+                if (q[i] - p[i] == D) return p[i];
+            }
+        }
+
+        return -1;
+    }
+};
+
+// CUT begin
+ifstream data("GridSpiral.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T>
+void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) { s = next_line(); }
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) { return "\"" + t + "\""; }
+
+bool do_test(int D, long long __expected) {
+    time_t startClock = clock();
+    GridSpiral *instance = new GridSpiral();
+    long long __result = instance->findCell(D);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!"
+             << " (" << elapsed << " seconds)" << endl;
+        return true;
+    } else {
+        cout << "FAILED!"
+             << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0) break;
+        int D;
+        from_stream(D);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if (do_test(D, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl
+             << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1594299414;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs"
+             << endl;
+        cout << "Score  : "
+             << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))
+             << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if (string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "GridSpiral (300 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
