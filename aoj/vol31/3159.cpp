52a78002a479b9a6527aa9415da91dcfdd12888c
imulan <cp.imulan@gmail.com>
2020-10-11 20:01:11 +0900

    Solve 3159

---
+++ aoj/vol31/3159.cpp
@@ -0,0 +1,119 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0;i<(int)(n);++i)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define dbg(x) cout<<#x" = "<<((x))<<endl
+template<class T,class U> ostream& operator<<(ostream& o, const pair<T,U> &p){o<<"("<<p.fi<<","<<p.se<<")";return o;}
+template<class T> ostream& operator<<(ostream& o, const vector<T> &v){o<<"[";for(T t:v){o<<t<<",";}o<<"]";return o;}
+// clang-format on
+
+template <int mod>
+struct ModInt {
+    int x;
+
+    ModInt() : x(0) {}
+    ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+    ModInt &operator+=(const ModInt &p) {
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+    ModInt &operator-=(const ModInt &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+    ModInt &operator*=(const ModInt &p) {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+    ModInt &operator/=(const ModInt &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+
+    ModInt operator-() const { return ModInt(-x); }
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+
+    ModInt inverse() const {
+        int a = x, b = mod, u = 1, v = 0, t;
+        while (b > 0) {
+            t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return ModInt(u);
+    }
+
+    ModInt pow(int64_t n) const {
+        ModInt ret(1), mul(x);
+        while (n > 0) {
+            if (n & 1) ret *= mul;
+            mul *= mul;
+            n >>= 1;
+        }
+        return ret;
+    }
+
+    friend ostream &operator<<(ostream &os, const ModInt &p) {
+        return os << p.x;
+    }
+
+    friend istream &operator>>(istream &is, ModInt &a) {
+        int64_t t;
+        is >> t;
+        a = ModInt<mod>(t);
+        return (is);
+    }
+
+    static int get_mod() { return mod; }
+};
+
+const int mod = 998244353;
+using mint = ModInt<mod>;
+
+int main() {
+    int n, m, k, f;
+    cin >> n >> m >> k >> f;
+
+    int ct = 0;
+    rep(i, n) rep(j, n) {
+        if (abs(i - j) <= 1) ++ct;
+    }
+    int nct = n * n - ct;
+
+    bool ok = true;
+    int dig_or = 0;
+    rep(i, m) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (abs(a - b) <= 1) {
+            --ct;
+            dig_or |= c;
+            if ((c | f) != f) ok = false;
+        } else
+            --nct;
+    }
+    if (ct == 0 && dig_or != f) ok = false;
+    if (!ok) {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    int p = __builtin_popcount(dig_or), q = __builtin_popcount(f);
+
+    mint x = mint(1 << p).pow(ct);
+    rep(i, q - p) x *= (mint(2).pow(ct) - 1);
+
+    cout << x * mint(1 << k).pow(nct) << "\n";
+    return 0;
+}
